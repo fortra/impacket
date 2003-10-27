6ecbc65ebe5a5438a2df3590955bd957e8d1c4a7
jkohen <jkohen@ad9a8d78-5211-11de-8950-a5d9c972bc64>
2003-10-27 17:36:57 +0000

    Changed source license to Apache.

--- trunk/impacket/dcerpc/__init__.py
+++ trunk/impacket/dcerpc/__init__.py
@@ -1,25 +1,9 @@
-#--
-# $CoreSDI: __init__.py,v 1.1.2.1 2003/07/30 17:03:53 jkohen Exp $
+# Copyright (c) 2003 CORE Security Technologies
 #
-# Copyright (c) 2003 CORE Security Technologies, CORE SDI Inc.
-# All rights reserved.
+# This software is provided under under a slightly modified version
+# of the Apache Software License. See the accompanying LICENSE file
+# for more information.
 #
-# This computer software is owned by Core SDI Inc. and is
-# protected by U.S. copyright laws and other laws and by international
-# treaties.  This computer software is furnished by CORE SDI Inc.
-# pursuant to a written license agreement and may be used, copied,
-# transmitted, and stored only in accordance with the terms of such
-# license and with the inclusion of the above copyright notice.  This
-# computer software or any other copies thereof may not be provided or
-# otherwise made available to any other person.
-#
-#
-# THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED
-# WARRANTIES ARE DISCLAIMED. IN NO EVENT SHALL CORE SDI Inc. BE LIABLE
-# FOR ANY DIRECT,  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY OR
-# CONSEQUENTIAL  DAMAGES RESULTING FROM THE USE OR MISUSE OF
-# THIS SOFTWARE
-#
-#--
+# $Id$
 
 pass
