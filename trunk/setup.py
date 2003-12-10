ead6ee8c86079e7d60b623550d72811d4a21ff8c
jkohen <jkohen@ad9a8d78-5211-11de-8950-a5d9c972bc64>
2003-12-10 23:42:47 +0000

    Bumped version number.

--- trunk/setup.py
+++ trunk/setup.py
@@ -9,7 +9,7 @@ from distutils.core import setup
 PACKAGE_NAME = "Impacket"
 
 setup(name = PACKAGE_NAME,
-      version = "0.9.4",
+      version = "0.9.5",
       description = "Network protocols Constructors and Dissectors",
       url = "http://oss.coresecurity.com/impacket",
       author = "CORE Security Technologies",
