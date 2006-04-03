e380f81f61d77547d940d6044f04d1d171c9e381
max <max@ad9a8d78-5211-11de-8950-a5d9c972bc64>
2006-04-03 15:52:03 +0000

    New release

--- trunk/setup.py
+++ trunk/setup.py
@@ -9,13 +9,13 @@ from distutils.core import setup
 PACKAGE_NAME = "Impacket"
 
 setup(name = PACKAGE_NAME,
-      version = "0.9.5.1",
+      version = "0.9.5.2",
       description = "Network protocols Constructors and Dissectors",
       url = "http://oss.coresecurity.com/projects/impacket.html",
       author = "CORE Security Technologies",
       author_email = "oss@coresecurity.com",
-      maintainer = "Javier Kohen",
-      maintainer_email = "jkohen@coresecurity.com",
+      maintainer = "Max Caceres",
+      maintainer_email = "max@coresecurity.com",
       packages = ['impacket', 'impacket.dcerpc'],
       scripts = glob.glob(os.path.join('examples', '*.py')),
       data_files = [(os.path.join('share', 'doc', PACKAGE_NAME),
