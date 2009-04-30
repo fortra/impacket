336d2e8a25fcff64de10c39a0c62ff2226690aa2
gera <gera@ad9a8d78-5211-11de-8950-a5d9c972bc64>
2009-04-30 21:29:16 +0000

    added some DHCP options, more to come

--- impacket/dhcp.py
+++ impacket/dhcp.py
@@ -34,16 +34,71 @@ class DhcpPacket(BootpPacket):
         
 
     options = {
-        'message-type':(53,'B'),
-        'auto-configuration':(116,'B'),
+        # 3. Vendor Extensions
+        'pad':(0,'_'),
+        'subnet-mask':(1,'!L'),
+        'time-offset':(2,'!L'),
+        'router':(3,'*!L'),
+        'time-server':(4,'*!L'),
+        'name-server':(5,'*!L'),
+        'domain-name-server':(6,'*!L'),
+        'log-server':(7,'*!L'),
+        'cookie-server':(8,'*!L'),
+        'lpr-server':(9,'*!L'),
+        'impress-server':(10,'*!L'),
+        'resource-locator-server':(11,'*!L'),
         'host-name':(12,':'),
-        'requested-ip':(50,'!L'),
-        'client-id':(61,':'),
-        'lease-time':(51,'!L'),
-        'vendor-class':(60,':'),
-        'parameter-request-list':(55,':'),
-        'fully-qualified-domain-name':(81,':'),
+        'boot-file-size':(13,'!H'),
+        'merit-dump-file':(14,':'),
+        'domain-name':(15,':'),
+        'swap-server':(16,':'),
+        'root-path':(17,':'),
+        'extensions-path':(18,':'),
+
+        # 4. IP Layer Parameters per Host
+        'ip-forwarding':(19,'B'),
+        'non-local-source-routing':(20,'B'),
+        'policy-filter':(21,'*!L'),
+        'maximum-datagram-reassembly-size':(22,'!H'),
+        'default-ip-ttl':(23,'B'),
+        'path-mtu-aging-timeout':(24,'!L'),
+        'path-mtu-plateau-table':(25,'*!H'),
+
+        # 5. IP Layer Parameters per Interface
+        'interface-mtu':(26,'!H'),
+        'all-subnets-are-local':(27,'B'),
+        'broadcast-address':(28,'!L'),
+        'perform-mask-discovery':(29,'B'),
+        'mask-supplier':(30,'B'),
+        'perform-router-discovery':(31,'B'),
+        'router-solicitation-address':(32,'!L'),
+        'static-route':(33,'*!L'),
+
+        # 6. Link Layer Parameters per Interface
+        'trailer-encapsulation':(34,'B'),
+        'arp-cache-timeout':(35,'!L'),
+        'ethernet-encapsulation':(36:'B'),
+
+        # 7. TCP parameters
+        'tcp-default-ttl':(37,'B'),
+        'tcp-keepalive-interval':(38,'!L'),
+        'tcp-keepalive-garbage':(39,'B'),
+
+        # 8. Application and Service parameters
+        'nis-domain':(40,':'),
+        'nis-servers':(41,'*!L'),
+        'ntp-servers':(42,'*!L'),
+
+
         'vendor-specific':(43,':'),
+        'requested-ip':(50,'!L'),
+        'lease-time':(51,'!L'),
+        'message-type':(53,'B'),
+        'parameter-request-list':(55,':'),
+        'vendor-class':(60,':'),
+        'client-id':(61,':'),
+        'fully-qualified-domain-name':(81,':'),
+        'auto-configuration':(116,'B'),
         'eof':(255,'_'),
 
     }
