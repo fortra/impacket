7846ef735ee08091522073658f06c9788f5c2d4c
narrowmind <narrowmind@ad9a8d78-5211-11de-8950-a5d9c972bc64>
2009-09-11 15:06:01 +0000

    Typo

--- trunk/impacket/dot11.py
+++ trunk/impacket/dot11.py
@@ -21,7 +21,7 @@ import types
 from ImpactPacket import ProtocolLayer, PacketBuffer
 from binascii import hexlify,crc32
 
-class Dot11ManagementCapability:
+class Dot11ManagementCapabilities:
     #
     # Capability Information
     #   0   1   2   3   4   5   6   7   8   9   A   B   C   D   E   F
