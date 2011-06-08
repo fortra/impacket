9679e09b967792caead7dc916ea06b3dfb691571
gmoreira <gmoreira@ad9a8d78-5211-11de-8950-a5d9c972bc64>
2011-06-08 02:37:58 +0000

    Updated maintainer info

--- trunk/setup.py
+++ trunk/setup.py
@@ -9,13 +9,13 @@ from distutils.core import setup
 PACKAGE_NAME = "Impacket"
 
 setup(name = PACKAGE_NAME,
-      version = "0.9.6.0",
+      version = "0.9.7.0",
       description = "Network protocols Constructors and Dissectors",
       url = "http://oss.coresecurity.com/projects/impacket.html",
       author = "CORE Security Technologies",
       author_email = "oss@coresecurity.com",
-      maintainer = "Max Caceres",
-      maintainer_email = "max@coresecurity.com",
+      maintainer = "Gustavo Moreira",
+      maintainer_email = "gmoreira@gmail.com",
       packages = ['impacket', 'impacket.dcerpc'],
       scripts = glob.glob(os.path.join('examples', '*.py')),
       data_files = [(os.path.join('share', 'doc', PACKAGE_NAME),
