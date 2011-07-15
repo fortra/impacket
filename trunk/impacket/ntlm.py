015e4ea666122142682c01c25cbb43c131e927b7
bethus@gmail.com <bethus@gmail.com@ad9a8d78-5211-11de-8950-a5d9c972bc64>
2011-07-15 18:42:58 +0000

    Fixed NTLMAuthChallenge in the situation I sent TargetInfoFields not as a structure but just as pure data

--- trunk/impacket/ntlm.py
+++ trunk/impacket/ntlm.py
@@ -253,7 +253,7 @@ class NTLMAuthChallenge(Structure):
         return 8
 
     def getData(self):
-        if self['TargetInfoFields'] is not None:
+        if self['TargetInfoFields'] is not None and type(self['TargetInfoFields']) is not str:
             raw_av_fields = self['TargetInfoFields'].getData()
             self['TargetInfoFields'] = raw_av_fields
         return Structure.getData(self)
