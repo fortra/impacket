941269eeb02531ae6270846441bffd74d0f90426
bethus@gmail.com <bethus@gmail.com@ad9a8d78-5211-11de-8950-a5d9c972bc64>
2011-12-26 20:03:24 +0000

    Adjusted to work with latest dcerpc.py

--- examples/ifmap.py
+++ examples/ifmap.py
@@ -301,6 +301,7 @@ def main(args):
   trans.set_dport(port)
 
   dce = dcerpc.DCERPC_v5(trans)
+  dce.set_credentials('test','cach0','')
   dce.connect()
 
   iid = uuid.uuidtup_to_bin(("afa8bd80-7d8a-11c9-bef4-08002b102989", "1.0"))
@@ -327,8 +328,8 @@ def main(args):
     try:
       dce.bind(binuuid)
     except dcerpc.Exception, e:
-      resp = e.args[1]
-      if (resp.get_result(), resp.get_result_reason()) == (2, 1):
+      resp = dcerpc.MSRPCBindAck(str(e.args[1]))
+      if (resp.getCtxItem(1)['Result'], resp.getCtxItem(1)['Reason']) == (2, 1):
         listening = False
       else:
         raise
