b594ba55562acb7fed3ad49ac4a147fef5ceb62a
bethus@gmail.com <bethus@gmail.com@ad9a8d78-5211-11de-8950-a5d9c972bc64>
2012-01-24 18:38:42 +0000

    CONFIG option finished

--- examples/services.py
+++ examples/services.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# Copyright (c) 2003 CORE Security Technologies
+# Copyright (c) 2003-2012 CORE Security Technologies
 #
 # This software is provided under under a slightly modified version
 # of the Apache Software License. See the accompanying LICENSE file
@@ -106,7 +106,48 @@ class SVCCTL:
         elif self.__action.upper() == 'CONFIG':
             print "Querying service config for %s" % self.__service_name
             resp = rpc.QueryServiceConfigW(serviceHandle)
-            #print repr(resp)
+            print "TYPE              : %2d - " % resp['QueryConfig']['ServiceType'],
+            if resp['QueryConfig']['ServiceType'] == 0x1:
+                print "SERVICE_KERNLE_DRIVER"
+            elif resp['QueryConfig']['ServiceType'] == 0x2:
+                print "SERVICE_FILE_SYSTEM_DRIVER"
+            elif resp['QueryConfig']['ServiceType'] == 0x10:
+                print "SERVICE_WIN32_OWN_PROCESS"
+            elif resp['QueryConfig']['ServiceType'] == 0x20:
+                print "SERVICE_WIN32_SHARE_PROCESS"
+            else:
+                print "UNKOWN"
+            print "START_TYPE        : %2d - " % resp['QueryConfig']['StartType'],
+            if resp['QueryConfig']['StartType'] == 0x0:
+                print "BOOT START"
+            elif resp['QueryConfig']['StartType'] == 0x1:
+                print "SYSTEM START"
+            elif resp['QueryConfig']['StartType'] == 0x2:
+                print "AUTO START"
+            elif resp['QueryConfig']['StartType'] == 0x3:
+                print "DEMAND START"
+            elif resp['QueryConfig']['StartType'] == 0x4:
+                print "DISABLED"
+            else:
+                print "UNKOWN"
+
+            print "ERROR_CONTROL     : %2d - " % resp['QueryConfig']['ErrorControl'],
+            if resp['QueryConfig']['ErrorControl'] == 0x0:
+                print "IGNORE"
+            elif resp['QueryConfig']['ErrorControl'] == 0x1:
+                print "NORMAL"
+            elif resp['QueryConfig']['ErrorControl'] == 0x2:
+                print "SEVERE"
+            elif resp['QueryConfig']['ErrorControl'] == 0x3:
+                print "CRITICAL"
+            else:
+                print "UNKOWN"
+            print "BINARY_PATH_NAME  : %s" % resp['QueryConfig']['BinaryPathName'].decode('utf-16le')
+            print "LOAD_ORDER_GROUP  : %s" % resp['QueryConfig']['LoadOrderGroup'].decode('utf-16le')
+            print "TAG               : %d" % resp['QueryConfig']['TagID']
+            print "DISPLAY_NAME      : %s" % resp['QueryConfig']['DisplayName'].decode('utf-16le')
+            print "DEPENDENCIES      : %s" % resp['QueryConfig']['Dependencies'].decode('utf-16le').replace('/',' - ')
+            print "SERVICE_START_NAME: %s" % resp['QueryConfig']['ServiceStartName'].decode('utf-16le')
         elif self.__action.upper() == 'STATUS':
             print "Querying status for %s" % self.__service_name
             resp = rpc.QueryServiceStatus(serviceHandle)
