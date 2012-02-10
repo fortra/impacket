edd1fb88e1dbb8067ab4cc11aec96d9d93e72059
bethus@gmail.com <bethus@gmail.com@ad9a8d78-5211-11de-8950-a5d9c972bc64>
2012-02-10 21:22:19 +0000

    First implementation of Workstation Service. Implemented NetrWkstaTransportEnum() for retrieving target systems MAC address and Interface Information

---
+++ trunk/impacket/dcerpc/wkssvc.py
@@ -0,0 +1,81 @@
+# Copyright (c) 2003-2011 CORE Security Technologies
+#
+# This software is provided under under a slightly modified version
+# of the Apache Software License. See the accompanying LICENSE file
+# for more information.
+#
+# $Id$
+#
+# Author: Alberto Solino
+#
+# Description:
+#   WKSSVC interface implementation.
+#
+
+from impacket.structure import Structure
+from impacket import dcerpc
+from impacket.dcerpc import ndrutils
+from impacket.uuid import uuidtup_to_bin
+
+MSRPC_UUID_WKSSVC = uuidtup_to_bin(('6BFFD098-A112-3610-9833-46C3F87E345A','1.0'))
+
+class WKSTA_TRANSPORT_INFO_0(Structure):
+    structure = (
+       ('UnUsed','<L'),
+       ('NumberOfRemoteConnections','<L'),
+       ('RefId1','<L'),
+       ('RefId2','<L'),
+       ('IsRoutableTransport','<L'),
+       ('TransportName',':',ndrutils.NDRStringW),
+       ('TransportAddress',':',ndrutils.NDRStringW),
+    )
+
+class WKSSVCNetrWkstaTransportEnum(Structure):
+    opnum = 5
+    alignment = 4
+    structure = (
+       ('ServerName',':',ndrutils.NDRUniqueStringW),
+       ('TransportInfo','20s'),
+       ('MaxBuffer','<L=0xffffffff'),
+       ('refId','<L=1'),
+       ('ResumeHandle','<L=0'),
+    )
+
+class WKSSVCNetrWkstaTransportEnumResponse(Structure):
+    structure = (
+       ('Level','<L'),
+       ('Case','<L'),
+       ('refId','<L'),
+       ('Count','<L'),
+       ('refId2','<L'),
+       ('MaxCount','<L'),
+       ('Array',':',WKSTA_TRANSPORT_INFO_0),
+       ('TotalEntries','<L'),
+       ('refId3','<L'),
+       ('ResumeHandle','<L'),
+       ('ErrorCode','<L')
+    )
+
+class DCERPCWksSvc:
+    def __init__(self, dcerpc):
+        self._dcerpc = dcerpc
+
+    def doRequest(self, request, noAnswer = 0, checkReturn = 1):
+        self._dcerpc.call(request.opnum, request)
+        if noAnswer:
+            return
+        else:
+            answer = self._dcerpc.recv()
+            if checkReturn and answer[-4:] != '\x00\x00\x00\x00':
+                raise Exception, 'DCE-RPC call returned an error.'
+            return answer
+
+    def NetrWkstaTransportEnum( self, serverName ):
+      transportEnum = WKSSVCNetrWkstaTransportEnum()
+      transportEnum['ServerName'] = ndrutils.NDRUniqueStringW()
+      transportEnum['ServerName']['Data'] = (serverName+'\x00').encode('utf-16le')
+      transportEnum['TransportInfo'] = '\x00'*8 + '\x04\x00\x04\x00' + '\x00'*8
+      data = self.doRequest(transportEnum, checkReturn = 1)
+      ans = WKSSVCNetrWkstaTransportEnumResponse(data)
+      return ans
+
