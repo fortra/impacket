dd7bb98ca08c8f1c4e776c8008d50fa50dbdd3e7
bethus@gmail.com <bethus@gmail.com@ad9a8d78-5211-11de-8950-a5d9c972bc64>
2012-02-13 13:55:02 +0000

    Quick and dirty fix so it now supports bash like history (using cmd.Cmd)

--- examples/smbclient.py
+++ examples/smbclient.py
@@ -35,34 +35,30 @@ import sys
 import string
 from impacket import smb
 from impacket.dcerpc import dcerpc_v4, dcerpc, transport, srvsvc
+import cmd
 
-class MiniImpacketShell:    
+class MiniImpacketShell(cmd.Cmd):    
     def __init__(self):
+        cmd.Cmd.__init__(self)
+        self.prompt = '# '
         self.smb = None
         self.tid = None
         self.pwd = ''
         self.share = None
 
-    def eval(self, s):
-        l = string.split(s, ' ')
-        cmd = l[0]
-        
-        try:        
-            f = MiniImpacketShell.__dict__[cmd]
-            l[0] = self
-            f(*l)
+    def onecmd(self,s):
+        retVal = False
+        try:
+           retVal = cmd.Cmd.onecmd(self,s)
         except Exception, e:
-            print "exception! %s" % e
+           print "ERROR: %s" % e
 
-    def run(self):
-        s = raw_input('# ')
-        while s:
-            if s == 'exit':
-              break
-            self.eval(s)
-            s = raw_input('# ')
+        return retVal
 
-    def help(self):
+    def do_exit(self,line):
+        return True
+
+    def do_help(self,line):
         print """
  open {host,port,remote_name = '*SMBSERVER'} - opens a SMB connection against the target host/port
  login {username,passwd,domain} - logs into the current SMB connection, no parameters for NULL connection
@@ -82,23 +78,51 @@ class MiniImpacketShell:
  close - closes the current SMB Session
  exit - terminates the server process (and this session)
 
- An empty line finishes the session
- NOTE: the server is not terminated, although it is left unusable
 """
 
-    def open(self,host,port, remote_name='*SMBSERVER'):
+    def do_open(self,line):
+        l = line.split(' ')
+        port = 445
+        if len(l) > 0:
+           host = l[0]
+        if len(l) > 1:
+           port = l[1]
+        if len(l) > 2:
+           remote_name = l[2]
+        else:
+           remote_name = '*SMBSERVER'
+
         self.smb = smb.SMB(remote_name, host, sess_port=int(port))
 
-    def login(self,username='', password='', domain=''):
+    def do_login(self,line):
+        l = line.split(' ')
+        username = ''
+        password = ''
+        domain = ''
+        if len(l) > 0:
+           username = l[0]
+        if len(l) > 1:
+           password = l[1]
+        if len(l) > 2:
+           domain = l[2]
+
         self.smb.login(username, password, domain=domain)
 
-    def login_hash(self,username, lmhash, nthash):
+    def do_login_hash(self,line): 
+        l = line.split(' ')
+        if len(l) > 0:
+           username = l[0]
+        if len(l) > 1:
+           lmhash = l[1]
+        if len(l) > 2:
+           nthash = l[2]
+
         self.smb.login(username, '', lmhash=lmhash, nthash=nthash)
 
-    def logoff(self):
+    def do_logoff(self, line):
         self.smb.logoff()
 
-    def info(self):
+    def do_info(self, line):
         rpctransport = transport.SMBTransport(self.smb.get_remote_name(), self.smb.get_remote_host(), filename = r'\srvsvc', smb_server = self.smb)
         dce = dcerpc.DCERPC_v5(rpctransport)
         dce.connect()                     
@@ -112,7 +136,7 @@ class MiniImpacketShell:
         print "Server UserPath: %s" % resp['UserPath']
         print "Simultaneous Users: %d" % resp['Users']
          
-    def shares(self):
+    def do_shares(self, line):
         rpctransport = transport.SMBTransport(self.smb.get_remote_name(), self.smb.get_remote_host(), filename = r'\srvsvc', smb_server = self.smb)
         dce = dcerpc.DCERPC_v5(rpctransport)
         dce.connect()                     
@@ -125,63 +149,63 @@ class MiniImpacketShell:
         #for share in self.smb.list_shared():
         #    print "%s" % share.get_name()
 
-    def use(self,sharename):
-        self.share = sharename
-        self.tid = self.smb.connect_tree(sharename)
+    def do_use(self,line):
+        self.share = line
+        self.tid = self.smb.connect_tree(line)
 
-    def cd(self, path):
-        p = string.replace(path,'/','\\')
+    def do_cd(self, line):
+        p = string.replace(line,'/','\\')
         if p[0] == '\\':
-           self.pwd = path
+           self.pwd = line
         else:
-           self.pwd += '/' + path
+           self.pwd += '/' + line
 
-    def pwd(self):
+    def do_pwd(self,line):
         print self.pwd
 
-    def ls(self, wildcard = None):
-        if wildcard == None:
+    def do_ls(self, wildcard):
+        if wildcard == '':
            pwd = self.pwd + '/*'
         else:
            pwd = self.pwd + '/' + wildcard
         for f in self.smb.list_path(self.share, pwd):
            print "%s" % f.get_longname()
 
-    def rm(self, filename):
+    def do_rm(self, filename):
         f = self.pwd + '/' + filename
         file = string.replace(f,'/','\\')
         self.smb.remove(self.share, file)
  
-    def mkdir(self, path):
+    def do_mkdir(self, path):
         p = self.pwd + '/' + path
         pathname = string.replace(p,'/','\\')
         self.smb.mkdir(self.share,pathname)
 
-    def rmdir(self, path):
+    def do_rmdir(self, path):
         p = self.pwd + '/' + path
         pathname = string.replace(p,'/','\\')
         self.smb.rmdir(self.share, pathname)
 
-    def put(self, filename):
+    def do_put(self, filename):
         fh = open(filename, 'rb')
         f = self.pwd + '/' + filename
         pathname = string.replace(f,'/','\\')
         self.smb.stor_file(self.share, pathname, fh.read)
         fh.close()
 
-    def get(self, filename):
+    def do_get(self, filename):
         fh = open(filename,'wb')
         f = self.pwd + '/' + filename
         pathname = string.replace(f,'/','\\')
         self.smb.retr_file(self.share, pathname, fh.write)
         fh.close()
 
-    def close(self):
-        self.smb.close();
+    def do_close(self, line):
+        del(self.smb);
 
 def main():
     shell = MiniImpacketShell()
-    shell.run()
+    shell.cmdloop()
 
 if __name__ == "__main__":
     main()
