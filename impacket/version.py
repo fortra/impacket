b236b79627b09b189e55f31ca47a83e4bc204a4b
bethus@gmail.com <bethus@gmail.com@ad9a8d78-5211-11de-8950-a5d9c972bc64>
2012-06-24 06:17:30 +0000

    Added version file used by the examples

---
+++ impacket/version.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2003-2012 CORE Security Technologies
+#
+# This software is provided under under a slightly modified version
+# of the Apache Software License. See the accompanying LICENSE file
+# for more information.
+#
+# $Id$
+#
+
+VER_MAJOR = "0"
+VER_MINOR = "9.9"
+
+BANNER = "Impacket v%s.%s - Copyright 2002-2012 Core Security Technologies\n" % (VER_MAJOR,VER_MINOR)
