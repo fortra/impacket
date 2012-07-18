1dca21e47ca242c584cc099a17a948912a2da023
bethus@gmail.com <bethus@gmail.com@ad9a8d78-5211-11de-8950-a5d9c972bc64>
2012-07-18 13:13:22 +0000

    Updated setup.py

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ from distutils.core import setup
 PACKAGE_NAME = "Impacket"
 
 setup(name = PACKAGE_NAME,
-      version = "0.9.7.0",
+      version = "0.9.9.0-dev",
       description = "Network protocols Constructors and Dissectors",
       url = "http://oss.coresecurity.com/projects/impacket.html",
       author = "CORE Security Technologies",
       author_email = "oss@coresecurity.com",
-      maintainer = "Gustavo Moreira",
-      maintainer_email = "gmoreira@gmail.com",
+      maintainer = "Alberto Solino",
+      maintainer_email = "bethus@gmail.com",
       packages = ['impacket', 'impacket.dcerpc'],
       scripts = glob.glob(os.path.join('examples', '*.py')),
       data_files = [(os.path.join('share', 'doc', PACKAGE_NAME),
