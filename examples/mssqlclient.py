cba4a5862e5d1703071e7742f267f7d2713c4fc6
bethus@gmail.com <bethus@gmail.com@ad9a8d78-5211-11de-8950-a5d9c972bc64>
2012-07-20 20:53:05 +0000

    Windows auth by default is False

--- examples/mssqlclient.py
+++ examples/mssqlclient.py
@@ -49,7 +49,7 @@ if __name__ == '__main__':
     parser.add_argument('target', action='store', help='[domain/][username[:password]@]<address>')
     parser.add_argument('-port', action='store', default='1433', help='target MSSQL port (default 1433)')
     parser.add_argument('-db', action='store', help='MSSQL database instance (default None)')
-    parser.add_argument('-windows-auth', action='store', choices=['True','False'], default = 'False', help='whether or not to use Windows Authentication (default True)')
+    parser.add_argument('-windows-auth', action='store', choices=['True','False'], default = 'False', help='whether or not to use Windows Authentication (default False)')
     
 
     group = parser.add_argument_group('authentication')
