964d3f208c4f729d6002e7d4845894db5e898344
bethus@gmail.com <bethus@gmail.com@ad9a8d78-5211-11de-8950-a5d9c972bc64>
2012-08-15 22:57:06 +0000

    Updated info

--- setup.py
+++ setup.py
@@ -9,9 +9,9 @@ from distutils.core import setup
 PACKAGE_NAME = "Impacket"
 
 setup(name = PACKAGE_NAME,
-      version = "0.9.9.0-dev",
+      version = "1.0.0.0-dev",
       description = "Network protocols Constructors and Dissectors",
-      url = "http://oss.coresecurity.com/projects/impacket.html",
+      url = "http://corelabs.coresecurity.com/index.php?module=Wiki&action=view&type=tool&name=Impacket",
       author = "CORE Security Technologies",
       author_email = "oss@coresecurity.com",
       maintainer = "Alberto Solino",
