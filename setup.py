ed906102e8a9cbcbccd2667a99d59b6782f5abeb
bethus@gmail.com <bethus@gmail.com@ad9a8d78-5211-11de-8950-a5d9c972bc64>
2013-04-30 19:17:17 +0000

    Some extra fields completed

--- setup.py
+++ setup.py
@@ -11,13 +11,17 @@ PACKAGE_NAME = "impacket"
 setup(name = PACKAGE_NAME,
       version = "1.0.0.0-dev",
       description = "Network protocols Constructors and Dissectors",
-      url = "http://corelabs.coresecurity.com/index.php?module=Wiki&action=view&type=tool&name=Impacket",
+      url = "http://oss.coresecurity.com/projects/impacket.html",
       author = "CORE Security Technologies",
       author_email = "oss@coresecurity.com",
       maintainer = "Alberto Solino",
       maintainer_email = "bethus@gmail.com",
+      license = "Apache modified",
+      long_description = 'Impacket is a collection of Python classes focused on providing access to network packets. Impacket allows Python developers to craft and decode network packets in simple and consistent manner.',
+      platforms = ["Unix","Windows"],
       packages = ['impacket', 'impacket.dcerpc', 'impacket.examples'],
       scripts = glob.glob(os.path.join('examples', '*.py')),
       data_files = [(os.path.join('share', 'doc', PACKAGE_NAME),
                      ['README', 'LICENSE']+glob.glob('doc/*'))],
+
       )
