70757ee97c21708b451a3179bec846fd2e81c980
bethus@gmail.com <bethus@gmail.com@ad9a8d78-5211-11de-8950-a5d9c972bc64>
2013-05-06 16:58:09 +0000

    Going back again to a development version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from distutils.core import setup
 PACKAGE_NAME = "impacket"
 
 setup(name = PACKAGE_NAME,
-      version = "0.9.10",
+      version = "1.0-dev",
       description = "Network protocols Constructors and Dissectors",
       url = "http://oss.coresecurity.com/projects/impacket.html",
       author = "CORE Security Technologies",
