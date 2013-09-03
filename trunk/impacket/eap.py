550fa9f874ef8444fb5605da09eee6b8a89a583a
aurelianocalvo@gmail.com <aurelianocalvo@gmail.com@ad9a8d78-5211-11de-8950-a5d9c972bc64>
2013-09-03 19:21:43 +0000

    ProtocolPackets for eap and wps. Including some basic tests.

---
+++ trunk/impacket/eap.py
@@ -0,0 +1,104 @@
+from helper import ProtocolPacket, Byte, Word, Long, ThreeBytesBigEndian, BaseDecoder
+from impacket.dot11 import SNAP
+import wps
+
+DOT1X_AUTHENTICATION = 0x888E
+
+class EAPExpanded(ProtocolPacket):
+    """EAP expanded data according to RFC 3748, section 5.7"""
+    
+    WFA_SMI = 0x00372a
+    SIMPLE_CONFIG = 0x00000001
+
+    header_size = 7
+    tail_size = 0
+    
+    vendor_id = ThreeBytesBigEndian(0)
+    vendor_type = Long(3, ">")
+
+class EAPR(ProtocolPacket):
+    """It represents a request or a response in EAP (codes 1 and 2)"""
+    
+    IDENTITY = 0x01
+    EXPANDED = 0xfe
+
+    header_size = 1
+    tail_size = 0
+    
+    type = Byte(0)            
+
+class EAP(ProtocolPacket):
+    REQUEST = 0x01
+    RESPONSE = 0x02
+    SUCCESS = 0x03
+    FAILURE = 0x04 
+
+    header_size = 4
+    tail_size = 0
+
+    code = Byte(0)
+    identifier = Byte(1)
+    length = Word(2, ">")        
+
+class EAPOL(ProtocolPacket):
+    EAP_PACKET = 0x00
+    EAPOL_START = 0x01
+    EAPOL_LOGOFF = 0x02
+    EAPOL_KEY = 0x03
+    EAPOL_ENCAPSULATED_ASF_ALERT = 0x04
+    
+    DOT1X_VERSION = 0x01
+
+    header_size = 4
+    tail_size = 0
+    
+    version = Byte(0)
+    packet_type = Byte(1)
+    body_length = Word(2, ">")
+    
+class EAPExpandedDecoder(BaseDecoder):
+    child_decoders = {
+        (EAPExpanded.WFA_SMI, EAPExpanded.SIMPLE_CONFIG): wps.SimpleConfigDecoder(),
+    }
+    klass = EAPExpanded
+    child_key = lambda s,p: (p.get_vendor_id(), p.get_vendor_type())
+        
+class EAPRDecoder(BaseDecoder):
+    child_decoders = {
+        EAPR.EXPANDED:EAPExpandedDecoder()
+    }
+    klass = EAPR
+    child_key = lambda s, p: p.get_type()
+        
+class EAPDecoder(BaseDecoder):
+    child_decoders = {
+        EAP.REQUEST: EAPRDecoder(),
+        EAP.RESPONSE: EAPRDecoder(),
+    }
+    klass = EAP
+    child_key = lambda s, p: p.get_code()
+        
+class EAPOLDecoder(BaseDecoder):
+    child_decoders = {
+        EAPOL.EAP_PACKET: EAPDecoder()
+    }
+    klass = EAPOL
+    child_key = lambda s, p: p.get_packet_type()
+    
+class EnhancedDecoder(object):
+    """Enhances a decoder so it handles an EAPOL packet inside a SNAP packet"""
+    
+    def __init__(self, base_decoder):
+        self.base_decoder = base_decoder
+    
+    def decode(self, buff):
+        packet = self.base_decoder.decode(buff)
+        snap = self.get_protocol(SNAP)
+        
+        if snap and snap.get_protoID() == DOT1X_AUTHENTICATION:
+            snap.contains(EAPOLDecoder().decode(snap.get_body_as_string()))
+            
+        return packet
+    
+    def get_protocol(self, p):
+        return self.base_decoder.get_protocol(p)
---
+++ trunk/impacket/helper.py
@@ -0,0 +1,147 @@
+from impacket.ImpactDecoder import Decoder, DataDecoder
+import impacket.ImpactPacket as ip
+import struct
+import functools
+
+def rebind(f):
+    functools.wraps(f)
+    def rebinder(*args, **kwargs):
+        return f(*args, **kwargs)
+        
+    return rebinder
+
+class Field(object):
+    def __init__(self, index):
+        self.index = index
+    
+    def __call__(self, k, d):
+        getter = rebind(self.getter)
+        getter_name = "get_" + k
+        getter.__name__ = getter_name
+        getter.__doc__ = "Get the %s field" % k
+        d[getter_name] = getter
+        
+        setter = rebind(self.setter)
+        setter_name = "set_" + k
+        setter.__name__ = setter_name
+        setter.__doc__ = "Set the %s field" % k
+        d["set_" + k] = setter
+        
+        d[k] = property(getter, setter, doc="%s property" % k)
+        
+class Bit(Field):
+    def __init__(self, index, bit_number):
+        Field.__init__(self, index)
+        self.mask = 2 ** bit_number
+        self.off_mask = (~self.mask) & 0xff
+        
+    def getter(self, o):
+        return (o.header.get_byte(self.index) & self.mask) != 0
+    
+    def setter(self, o, value=True):
+        b = o.header.get_byte(self.index)
+        if value:
+            b |= self.mask
+        else:
+            b &= self.off_mask
+        
+        o.header.set_byte(self.index, b) 
+
+class Byte(Field):
+    
+    def __init__(self, index):
+        Field.__init__(self, index)
+        
+    def getter(self, o):
+        return o.header.get_byte(self.index)
+    
+    def setter(self, o, value):
+        o.header.set_byte(self.index, value)
+        
+class Word(Field):
+    def __init__(self, index, order="!"):
+        Field.__init__(self, index)
+        self.order = order
+        
+    def getter(self, o):
+        return o.header.get_word(self.index, self.order)
+    
+    def setter(self, o, value):
+        o.header.set_word(self.index, value, self.order)
+
+class Long(Field):        
+    def __init__(self, index, order="!"):
+        Field.__init__(self, index)        
+        self.order = order
+        
+    def getter(self, o):
+        return o.header.get_long(self.index, self.order)
+    
+    def setter(self, o, value):
+        o.header.set_long(self.index, value, self.order)
+        
+class ThreeBytesBigEndian(Field):
+    def __init__(self, index):
+        Field.__init__(self, index)
+                
+    def getter(self, o):
+        b=o.header.get_bytes()[self.index:self.index+3].tostring()
+        #unpack requires a string argument of length 4 and b is 3 bytes long
+        (value,)=struct.unpack('!L', '\x00'+b)
+        return value
+
+    def setter(self, o, value):
+        # clear the bits
+        mask = ((~0xFFFFFF00) & 0xFF)
+        masked = o.header.get_long(self.index, ">") & mask
+        # set the bits 
+        nb = masked | ((value & 0x00FFFFFF) << 8)
+        o.header.set_long(self.index, nb, ">")
+
+
+class ProtocolPacketMetaklass(type):
+    def __new__(cls, name, bases, d):
+        d["_fields"] = []
+        items = d.items()
+        if not object in bases:
+            bases += (object,)
+        for k,v in items:
+            if isinstance(v, Field):
+                d["_fields"].append(k) 
+                v(k, d)
+                
+        d["_fields"].sort()
+        
+        def _fields_repr(self):
+            return " ".join( "%s:%s" % (f, repr(getattr(self, f))) for f in self._fields )
+        def __repr__(self):
+            
+            return "<%(name)s %(fields)s \nchild:%(r_child)s>" % {
+                "name": name,
+                "fields": self._fields_repr(),
+                "r_child": repr(self.child()), 
+            }
+        
+        d["_fields_repr"] = _fields_repr
+        d["__repr__"] = __repr__
+        
+        return type.__new__(cls, name, bases, d)
+
+class ProtocolPacket(ip.ProtocolPacket):
+    __metaclass__ = ProtocolPacketMetaklass  
+
+    def __init__(self, buff = None):
+        ip.ProtocolPacket.__init__(self, self.header_size, self.tail_size)
+        if buff:
+            self.load_packet(buff)
+            
+class BaseDecoder(Decoder):
+    
+    def decode(self, buff):
+        
+        packet = self.klass(buff)
+        self.set_decoded_protocol(packet)
+        cd = self.child_decoders.get(self.child_key(packet), DataDecoder())
+        packet.contains(cd.decode(packet.get_body_as_string()))
+        return packet
+    
