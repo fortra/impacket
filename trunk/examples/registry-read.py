f924c104690a79a7ccec55e8229040ad3b8b1f13
bethus <bethus@ad9a8d78-5211-11de-8950-a5d9c972bc64>
2013-09-20 15:19:24 +0000

    Example for windows registry reader.. you will need a registry file (you can grab it with ntfs-read)

---
+++ trunk/examples/registry-read.py
@@ -0,0 +1,153 @@
+# Copyright (c) 2003-2013 CORE Security Technologies)
+#
+# This software is provided under under a slightly modified version
+# of the Apache Software License. See the accompanying LICENSE file
+# for more information.
+#
+# $Id$
+#
+# Author: Alberto Solino (beto@coresecurity.com)
+#
+# Description: A Windows Registry Reader Example
+# 
+# Reference for:
+#  winregistry.py
+#
+
+import impacket
+from impacket import version
+from impacket import winregistry
+import sys
+import argparse
+import ntpath
+
+def bootKey(reg):
+    baseClass = 'ControlSet001\\Control\\Lsa\\'
+    keys = ['JD','Skew1','GBG','Data']
+    tmpKey = ''
+
+    for key in keys:
+        tmpKey = tmpKey + reg.getClass(baseClass + key).decode('utf-16le')[:8].decode('hex')
+        
+    transforms = [ 8, 5, 4, 2, 11, 9, 13, 3, 0, 6, 1, 12, 14, 10, 15, 7 ]
+
+    syskey = ''
+    for i in xrange(len(tmpKey)):
+        syskey += tmpKey[transforms[i]]
+
+    print syskey.encode('hex')
+
+def getClass(reg, className):
+    regKey = ntpath.dirname(className)
+    regClass = ntpath.basename(className)
+
+    value = reg.getClass(className)
+
+    if value is None:
+        return
+
+    print "[%s]" % regKey
+
+    print "Value for Class %s: \n" % regClass,
+
+    winregistry.hexdump(value,'   ')
+
+def getValue(reg, keyValue):
+    regKey = ntpath.dirname(keyValue)
+    regValue = ntpath.basename(keyValue)
+
+    value = reg.getValue(keyValue)
+
+    print "[%s]\n" % regKey
+
+    if value is None:
+        return
+
+    print "Value for %s:\n    " % regValue,
+    reg.printValue(value[0],value[1])
+
+def enumValues(reg, searchKey):
+    key = reg.findKey(searchKey)
+
+    if key is None:
+        return
+
+    print "[%s]\n" % searchKey
+
+    values = reg.enumValues(key)
+
+    for value in values:
+        print "  %s: " % (value),
+        data = reg.getValue('%s\\%s'%(searchKey,value))
+        # Special case for binary string.. so it looks better formatted
+        if data[0] == winregistry.REG_BINARY:
+            print ''
+            reg.printValue(data[0],data[1])
+            print ''
+        else:
+            reg.printValue(data[0],data[1])
+
+def enumKey(reg, searchKey, isRecursive, indent='  '):
+    #reg.walk()
+    parentKey = reg.findKey(searchKey)
+
+    if parentKey is None:
+        return
+
+    keys = reg.enumKey(parentKey)
+
+    for key in keys:
+        print "%s%s" %(indent, key)
+        if isRecursive is True:
+            if searchKey == '\\':
+                enumKey(reg, '\\%s'%(key),isRecursive,indent+'  ')
+            else:
+                enumKey(reg, '%s\\%s'%(searchKey,key),isRecursive,indent+'  ')
+
+
+def main():
+    print version.BANNER
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-file', action='store', help='registry hive to open')
+    subparsers = parser.add_subparsers(help='actions', dest='action')
+    # A enum_key command
+    enumkey_parser = subparsers.add_parser('enum_key', help='enumerates the subkeys of the specified open registry key')
+    enumkey_parser.add_argument('-name', action='store', required=True, help='registry key')
+    enumkey_parser.add_argument('-recursive', dest='recursive', action='store_true', required=False, help='recursive search (default False)')
+
+    # A enum_values command
+    enumvalues_parser = subparsers.add_parser('enum_values', help='enumerates the values for the specified open registry key')
+    enumvalues_parser.add_argument('-name', action='store', required=True, help='registry key')
+
+    # A get_value command
+    getvalue_parser = subparsers.add_parser('get_value', help='retrieves the data for the specified registry value')
+    getvalue_parser.add_argument('-name', action='store', required=True, help='registry value')
+
+    # A get_value command
+    getclass_parser = subparsers.add_parser('get_class', help='retrieves the data for the specified registry class')
+    getclass_parser.add_argument('-name', action='store', required=True, help='registry class name')
+
+    if len(sys.argv)==1:
+        parser.print_help()
+        sys.exit(1)
+
+    options = parser.parse_args()
+
+    reg = winregistry.Registry(options.file)
+
+    if options.action.upper() == 'ENUM_KEY':
+        print "[%s]" % options.name
+        enumKey(reg, options.name, options.recursive)
+    elif options.action.upper() == 'ENUM_VALUES':
+        enumValues(reg, options.name)
+    elif options.action.upper() == 'GET_VALUE':
+        getValue(reg, options.name)
+    elif options.action.upper() == 'GET_CLASS':
+        getClass(reg, options.name)
+
+    reg.close()
+
+if __name__ == "__main__":
+    main()
+
