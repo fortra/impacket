f91ad83aeb355434227f97eeac2c226e9400cb6c
bethus@gmail.com <bethus@gmail.com@ad9a8d78-5211-11de-8950-a5d9c972bc64>
2013-10-25 22:24:58 +0000

    1) NDRConformantVaryingArray added to ndrutils 2) LsarQuerySecret, LsarRetrievePrivateData, LsarOpenSecret added to lsarpc 3) Fixed tab spacing and documentation

--- impacket/dcerpc/lsarpc.py
+++ impacket/dcerpc/lsarpc.py
@@ -17,6 +17,7 @@ from impacket.structure import Structure
 from impacket.dcerpc import ndrutils
 from impacket.dcerpc.samr import SAMR_RPC_SID_IDENTIFIER_AUTHORITY, SAMR_RPC_SID
 from impacket.uuid import uuidtup_to_bin
+from impacket.nt_errors import ERROR_MESSAGES
 import random
 from struct import pack, unpack
 
@@ -51,6 +52,64 @@ LsapLookupXForestResolve          = 6
 LsapLookupRODCReferralToFullDC    = 7
 
 # Structs
+class LSAPR_CR_CIPHER_VALUE(Structure):
+    structure = (
+        ('Length','<L=0'),
+        ('MaximumLength','<L=0'),
+        ('pBuffer',':', ndrutils.NDRPointerNew ),
+        ('Buffer',':', ndrutils.NDRConformantVaryingArray),
+    )
+
+class LSARPCQuerySecret(Structure):
+    opnum = 30
+    alignment = 4
+    structure = (
+        ('SecretHandle','20s'),
+        ('EncryptedCurrentValue','<Q=1'),
+        ('pCurrentValueSetTime','<L=2'),
+        ('CurrentValueSetTime','<Q=0'),
+        ('EncryptedOldValue','L=0'),
+        ('OldValueSetTime','<Q=0'),
+    )
+
+class LSARPCQuerySecretResponse(Structure):
+    #alignment = 8 
+    structure = (
+        ('pEncryptedCurrentValue',':', ndrutils.NDRPointerNew),
+        ('pEncryptedCurrentValue2',':', ndrutils.NDRPointerNew),
+        ('EncryptedCurrentValue',':', LSAPR_CR_CIPHER_VALUE),
+        ('pCurrentValueSetTime',':', ndrutils.NDRPointerNew),
+        ('CurrentValueSetTime','<Q=0'),
+    )
+
+class LSARPCRetrievePrivateData(Structure):
+    opnum = 43
+    alignment = 4
+    structure = (
+        ('PolicyHandle','20s'),
+        ('KeyName',':'),
+        ('EncryptedData', '<L=0'),
+    )
+
+class LSARPCRetrievePrivateDataResponse(Structure):
+    structure = (
+        ('pEncryptedData', ':', ndrutils.NDRPointerNew),
+        ('EncryptedData', ':', LSAPR_CR_CIPHER_VALUE),
+    )
+
+class LSARPCOpenSecret(Structure):
+    opnum = 28
+    alignment = 4
+    structure = (
+        ('PolicyHandle','20s'),
+        ('SecretName',':'),
+        ('DesiredAccess','<L=0'),
+    )
+
+class LSARPCOpenSecretResponse(Structure):
+    structure = (
+        ('SecretHandle', '20s'),
+    )
 
 class LSARPCSetSystemAccessAccount(Structure):
     opnum = 24
@@ -352,11 +411,6 @@ class LSARPCLookupSidsResponse(Structure):
       return l_dict
 
 class LSARPCSessionError(Exception):
-
-    # TODO, complete the error codes here. Looks like LSA return NT Error codes
-    error_messages = {
-    }
-
     def __init__( self, error_code):
         Exception.__init__(self)
         self.error_code = error_code
@@ -366,10 +420,8 @@ class LSARPCSessionError(Exception):
 
     def __str__( self ):
         key = self.error_code
-        if (LSARPCSessionError.error_messages.has_key(key)):
-            error_msg_short = LSARPCSessionError.error_messages[key][0]
-            error_msg_verbose = LSARPCSessionError.error_messages[key][1] 
-            return 'LSARPC SessionError: code: %s - %s - %s' % (str(self.error_code), error_msg_short, error_msg_verbose)
+        if ERROR_MESSAGES.has_key(key):
+            return 'LSARPC SessionError: %s(%s)' % (ERROR_MESSAGES[self.error_code])
         else:
             return 'LSARPC SessionError: unknown error code: 0x%x' % (self.error_code)
 
@@ -390,143 +442,165 @@ class DCERPCLsarpc:
             return answer
 
     def LsarOpenPolicy2( self, systemName, desiredAccess = 0x00020801):
-      """
-       opens a context handle to the RPC server
+        """
+        opens a context handle to the RPC server
 
-       :param string systemName: This parameter does not have any effect on message processing in any environment. It MUST be ignored on receipt.
-       :param int desiredAccess: An ACCESS_MASK value that specifies the requested access rights that MUST be granted on the returned PolicyHandle if the request is successful. Check [MS-DTYP], section 2.4.3
-
-      """
-      open_policy = LSARPCOpenPolicy2()
-      open_policy['ServerName'] = ndrutils.NDRUniqueStringW()
-      open_policy['ServerName']['Data'] = (systemName+'\x00').encode('utf-16le')
-      #TODO: Implement ObjectAtributes structure
-      open_policy['ObjectAttributes'] = '\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
-      open_policy['AccessMask'] = desiredAccess
-      data = self.doRequest(open_policy)
-      ans = LSARPCOpenPolicy2Response(data)
-      return ans
+        :param string systemName: This parameter does not have any effect on message processing in any environment. It MUST be ignored on receipt.
+        :param int desiredAccess: An ACCESS_MASK value that specifies the requested access rights that MUST be granted on the returned PolicyHandle if the request is successful. Check [MS-DTYP], section 2.4.3
+  
+        :return: a structure with a policy handle, call dump() to check its structure. Otherwise raises an error
+        """
+        open_policy = LSARPCOpenPolicy2()
+        open_policy['ServerName'] = ndrutils.NDRUniqueStringW()
+        open_policy['ServerName']['Data'] = (systemName+'\x00').encode('utf-16le')
+        #TODO: Implement ObjectAtributes structure
+        open_policy['ObjectAttributes'] = '\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+        open_policy['AccessMask'] = desiredAccess
+        data = self.doRequest(open_policy)
+        ans = LSARPCOpenPolicy2Response(data)
+        return ans
 
     def LsarLookupSids( self, context_handle, sids):
-      '''
+        """
+        translates a batch of security principal SIDs to their name forms. It also returns the domains that these names are a part of.
 
-      :param HANDLE context_handle: OpenPolicy2 handle)
-      :param list sids: list of sids to look information for ([S1, S2 ...])
-      '''
+        :param HANDLE context_handle: OpenPolicy2 handle
+        :param list sids: list of sids to look information for ([S1, S2 ...])
 
-      open_policy = LSARPCLookupSids()
-      open_policy['ContextHandle'] = context_handle
-      open_policy['SidsBuff'] = SIDS_BUFF()
-      open_policy['SidsBuff']['NumSids'] = len(sids)
-      open_policy['SidsBuff']['RefID'] = random.randint(1,65535)
-      open_policy['SidsBuff']['MaxCount'] = len(sids)
+        :return: a structure with a list of translated sids, call dump() to see its contents. Otherwise it raises an error
+        """
+
+        open_policy = LSARPCLookupSids()
+        open_policy['ContextHandle'] = context_handle
+        open_policy['SidsBuff'] = SIDS_BUFF()
+        open_policy['SidsBuff']['NumSids'] = len(sids)
+        open_policy['SidsBuff']['RefID'] = random.randint(1,65535)
+        open_policy['SidsBuff']['MaxCount'] = len(sids)
       
-      sids_str = ''
-      sid_items = 0
-      for sid_i in range(len(sids)):
-        sid_arr = sids[sid_i].split('-')
-        _sid = SAMR_RPC_SID_STRUCT()
-        sid_items += 1
-        _sid['Count'] = len(sid_arr) - 3
-        _sid['Sid'] = SAMR_RPC_SID()
-        _sid['Sid']['Revision'] = int(sid_arr[1])
-        _sid['Sid']['SubAuthorityCount'] =len(sid_arr) - 3
-        _sid['Sid']['IdentifierAuthority'] = SAMR_RPC_SID_IDENTIFIER_AUTHORITY()
-        _sid['Sid']['IdentifierAuthority']['Value'] = '\x00\x00\x00\x00\x00' + pack('B',int(sid_arr[2]))
+        sids_str = ''
+        sid_items = 0
+        for sid_i in range(len(sids)):
+          sid_arr = sids[sid_i].split('-')
+          _sid = SAMR_RPC_SID_STRUCT()
+          sid_items += 1
+          _sid['Count'] = len(sid_arr) - 3
+          _sid['Sid'] = SAMR_RPC_SID()
+          _sid['Sid']['Revision'] = int(sid_arr[1])
+          _sid['Sid']['SubAuthorityCount'] =len(sid_arr) - 3
+          _sid['Sid']['IdentifierAuthority'] = SAMR_RPC_SID_IDENTIFIER_AUTHORITY()
+          _sid['Sid']['IdentifierAuthority']['Value'] = '\x00\x00\x00\x00\x00' + pack('B',int(sid_arr[2]))
 
-        sub_auth = ''
-        for elem in sid_arr[3:]:
-            sub_auth += pack('<L', int(elem))
-        _sid['Sid']['SubAuthority'] = sub_auth
+          sub_auth = ''
+          for elem in sid_arr[3:]:
+              sub_auth += pack('<L', int(elem))
+          _sid['Sid']['SubAuthority'] = sub_auth
+  
+          sids_str += _sid.getData()
 
-        sids_str += _sid.getData()
+        for i in range(0, sid_items):
+          sids_str = pack('<L',random.randint(1,65535)) + sids_str
+        open_policy['SidsBuff']['Sids'] = sids_str
+  
+        open_policy['TransNames'] = '\x00\x00\x00\x00\x00\x00\x00\x00'
+        open_policy['LookupLevel'] = 1
+        open_policy['MappedCount'] = '\x00\x00\x00\x00\x00\x00'
 
-      for i in range(0, sid_items):
-        sids_str = pack('<L',random.randint(1,65535)) + sids_str
-      open_policy['SidsBuff']['Sids'] = sids_str
-
-      open_policy['TransNames'] = '\x00\x00\x00\x00\x00\x00\x00\x00'
-      open_policy['LookupLevel'] = 1
-      open_policy['MappedCount'] = '\x00\x00\x00\x00\x00\x00'
-
-      data = self.doRequest(open_policy, checkReturn = 0)
-      packet = LSARPCLookupSidsResponse(data)
-      return packet
+        data = self.doRequest(open_policy, checkReturn = 0)
+        packet = LSARPCLookupSidsResponse(data)
+        return packet
 
     def LsarLookupSids3( self, context_handle, sids):
-      '''
-           This method receives the following parameters:
-                - Handle(OpenPolicy2 handle)
-                - list of sids to look information for ([S1, S2 ...])
-      '''
+        """
+        translates a batch of security principal SIDs to their name forms. It also returns the domains that these names are a part of.
+  
+        :param HANDLE context_handle: OpenPolicy2 handle
+        :param list sids: list of sids to look information for ([S1, S2 ...])
 
-      open_policy = LSARPCLookupSids3()
-      open_policy['ContextHandle'] = context_handle
-      open_policy['SidsBuff'] = SIDS_BUFF()
-      open_policy['SidsBuff']['NumSids'] = len(sids)
-      open_policy['SidsBuff']['RefID'] = random.randint(1,65535)
-      open_policy['SidsBuff']['MaxCount'] = len(sids)
+        :return: a structure with a list of translated sids, call dump() to see its contents. Otherwise it raises an error
+        """
+
+        open_policy = LSARPCLookupSids3()
+        open_policy['ContextHandle'] = context_handle
+        open_policy['SidsBuff'] = SIDS_BUFF()
+        open_policy['SidsBuff']['NumSids'] = len(sids)
+        open_policy['SidsBuff']['RefID'] = random.randint(1,65535)
+        open_policy['SidsBuff']['MaxCount'] = len(sids)
       
-      sids_str = ''
-      sid_items = 0
-      for sid_i in range(len(sids)):
-        sid_arr = sids[sid_i].split('-')
-        _sid = SAMR_RPC_SID_STRUCT()
-        sid_items += 1
-        _sid['Count'] = len(sid_arr) - 3
-        _sid['Sid'] = SAMR_RPC_SID()
-        _sid['Sid']['Revision'] = int(sid_arr[1])
-        _sid['Sid']['SubAuthorityCount'] =len(sid_arr) - 3
-        _sid['Sid']['IdentifierAuthority'] = SAMR_RPC_SID_IDENTIFIER_AUTHORITY()
-        _sid['Sid']['IdentifierAuthority']['Value'] = '\x00\x00\x00\x00\x00' + pack('B',int(sid_arr[2]))
-
-        sub_auth = ''
-        for elem in sid_arr[3:]:
-            sub_auth += pack('<L', int(elem))
-        _sid['Sid']['SubAuthority'] = sub_auth
-
-        sids_str += _sid.getData()
-
-      for i in range(0, sid_items):
-        sids_str = pack('<L',random.randint(1,65535)) + sids_str
-      open_policy['SidsBuff']['Sids'] = sids_str
-
-      open_policy['TransNames'] = '\x00\x00\x00\x00\x00\x00\x00\x00'
-      open_policy['LookupLevel'] = 1
-      open_policy['MappedCount'] = '\x00\x00\x00\x00\x00\x00'
-
-      data = self.doRequest(open_policy, checkReturn = 0)
-      packet = LSARPCLookupSidsResponse(data)
-      return packet
+        sids_str = ''
+        sid_items = 0
+        for sid_i in range(len(sids)):
+          sid_arr = sids[sid_i].split('-')
+          _sid = SAMR_RPC_SID_STRUCT()
+          sid_items += 1
+          _sid['Count'] = len(sid_arr) - 3
+          _sid['Sid'] = SAMR_RPC_SID()
+          _sid['Sid']['Revision'] = int(sid_arr[1])
+          _sid['Sid']['SubAuthorityCount'] =len(sid_arr) - 3
+          _sid['Sid']['IdentifierAuthority'] = SAMR_RPC_SID_IDENTIFIER_AUTHORITY()
+          _sid['Sid']['IdentifierAuthority']['Value'] = '\x00\x00\x00\x00\x00' + pack('B',int(sid_arr[2]))
+  
+          sub_auth = ''
+          for elem in sid_arr[3:]:
+              sub_auth += pack('<L', int(elem))
+          _sid['Sid']['SubAuthority'] = sub_auth
+  
+          sids_str += _sid.getData()
+  
+        for i in range(0, sid_items):
+          sids_str = pack('<L',random.randint(1,65535)) + sids_str
+        open_policy['SidsBuff']['Sids'] = sids_str
+  
+        open_policy['TransNames'] = '\x00\x00\x00\x00\x00\x00\x00\x00'
+        open_policy['LookupLevel'] = 1
+        open_policy['MappedCount'] = '\x00\x00\x00\x00\x00\x00'
+  
+        data = self.doRequest(open_policy, checkReturn = 0)
+        packet = LSARPCLookupSidsResponse(data)
+        return packet
 
     def LsarQueryInformationPolicy2(self, policyHandle, informationClass):
-       queryInfo = LSARPCQueryInformationPolicy2()
-       queryInfo['ContextHandle'] = policyHandle
-       queryInfo['InformationClass'] = informationClass
-       packet = self.doRequest(queryInfo)
-       
-       data = LSARPCQueryInformationPolicy2Response(packet)
-       # For the answers we can parse, we return the structs, for the rest, just the data
-       if informationClass == POLICY_PRIMARY_DOMAIN_INFORMATION:
-           return DOMAIN_INFORMATION(data['pRespBuffer'])
-       elif informationClass == POLICY_ACCOUNT_DOMAIN_INFORMATION:
-           return DOMAIN_INFORMATION(data['pRespBuffer'])
-       else:
-           return data
+        """
+        query values that represent the server's security policy
+ 
+        :param HANDLE policyHandle: OpenPolicy2 handle
+        :param int informationClass: the information class type requests. Check [MS-LSAD], section 3.1.4.4.3. Currently supported POLICY_PRIMARY_DOMAIN_INFORMATION and POLICY_ACCOUNT_DOMAIN_INFORMATION.
+ 
+        :return: a structure with the requested information class. Call the dump() method to check its structure. Otherwise raises an error
+        """
+        queryInfo = LSARPCQueryInformationPolicy2()
+        queryInfo['ContextHandle'] = policyHandle
+        queryInfo['InformationClass'] = informationClass
+        packet = self.doRequest(queryInfo)
+        
+        data = LSARPCQueryInformationPolicy2Response(packet)
+        # For the answers we can parse, we return the structs, for the rest, just the data
+        if informationClass == POLICY_PRIMARY_DOMAIN_INFORMATION:
+            return DOMAIN_INFORMATION(data['pRespBuffer'])
+        elif informationClass == POLICY_ACCOUNT_DOMAIN_INFORMATION:
+            return DOMAIN_INFORMATION(data['pRespBuffer'])
+        else:
+            return data
 
     def LsarClose( self, context_handle):
-      open_policy = LSARPCClose()
-      open_policy['ContextHandle'] = context_handle
-      data = self.doRequest(open_policy)
-      ans = LSARPCCloseResponse(data)
-      return ans
+        """
+        frees the resources held by a context handle that was opened earlier
+  
+        :param HANDLE context_handle: OpenPolicy2 handle
+  
+        :return: NULL or raises an exception on error
+        """
+        open_policy = LSARPCClose()
+        open_policy['ContextHandle'] = context_handle
+        data = self.doRequest(open_policy)
+        ans = LSARPCCloseResponse(data)
+        return ans
 
     def LsarLookupNames2(self, policyHandle, names, lookupLevel=LsapLookupWksta, lookupOptions = 0x0, clientRevision = 0x1):
         """
         translates a batch of security principal names to their SID form
 
         :param HANDLE policyHandle: OpenPolicy2 handle
-        :param string names: contains the security principal names to translate (only supports one name)
+        :param UNICODE names: contains the security principal names to translate (only supports one name)
         :param int lookupLevel: Specifies what scopes are to be used during translation, as specified in section 2.2.16 [MS-LSAT]
         :param int lookupOptions: flags that control the lookup operation. For possible values and their meanings, see section 3.1.4.5 [MS-LSAT]
         :param int clientRevision: version of the client, which implies the client's capabilities. For possible values and their meanings, see section 3.1.4.5 [MS-LSAT]
@@ -624,4 +698,70 @@ class DCERPCLsarpc:
         data = self.doRequest(setSystemAccess)
 
         return data
+
+    def LsarOpenSecret(self, policyHandle, secretName, desiredAccess=0x02000000):
+        """
+        sets system access account flags for an account object.
+
+        :param HANDLE policyHandle: OpenPolicy2 handle
+        :param UNICODE secretName: the name of the secret to open
+        :param int desiredAccess: An ACCESS_MASK value that specifies the requested access rights that MUST be granted on the returned PolicyHandle if the request is successful. Check [MS-DTYP], section 2.4.3
+
+        :return: returns the SecretHandle for the opened secret. Call dump() method to see the structure.
+        """
+        openSecret = LSARPCOpenSecret()
+        openSecret['PolicyHandle'] = policyHandle
+        rpcUnicodePtr = ndrutils.pRPC_UNICODE_STRING()
+        rpcUnicodePtr.setDataLen(secretName)
+        rpcUnicode = ndrutils.RPC_UNICODE_STRING()
+        rpcUnicode['Data'] = secretName
+        openSecret['SecretName'] = str(rpcUnicodePtr) + str(rpcUnicode)
+        openSecret['DesiredAccess'] = desiredAccess
+
+        data = self.doRequest(openSecret)
+        ans = LSARPCOpenSecretResponse(data)
+
+        return ans
+
+    def LsarRetrievePrivateData(self, policyHandle, keyName):
+        """
+        retrieves a secret value.
+
+        :param HANDLE policyHandle: OpenPolicy2 handle
+        :param UNICODE keyName: the name of the secret to retrieve
+
+        :return: returns a structure with the secret. Call dump() method to see the structure. Raises an exception on error
+        You can decrypt the secrets using crypto.decryptSecret(). You will need the sessionKey from the SMBConnection as the key for decryption (getSessionKey()).
+        """
+        retrievePrivateData = LSARPCRetrievePrivateData()
+        retrievePrivateData['PolicyHandle'] = policyHandle
+        rpcUnicodePtr = ndrutils.pRPC_UNICODE_STRING()
+        rpcUnicodePtr.setDataLen(keyName)
+        rpcUnicode = ndrutils.RPC_UNICODE_STRING()
+        rpcUnicode['Data'] = keyName
+        retrievePrivateData['KeyName'] = str(rpcUnicodePtr) + str(rpcUnicode)
+
+        data = self.doRequest(retrievePrivateData)
+        ans = LSARPCRetrievePrivateDataResponse(data)
+
+        return ans
+
+    def LsarQuerySecret(self, secretHandle):
+        """
+        retrieves the current and old (or previous) value of the secret object.
+
+        :param HANDLE secretHandle: LsarOpenSecret handle
+
+        :return: returns a structure with the secret. Call dump() method to see the structure. Raises an exception on error
+        """
+        querySecret = LSARPCQuerySecret()
+        querySecret['SecretHandle'] = secretHandle
+
+        data = self.doRequest(querySecret)
+        ans = LSARPCQuerySecretResponse(data)
+        ans.dump()
+
+        return ans
+
+
 
