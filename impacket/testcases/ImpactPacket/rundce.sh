99881090ed9ee2682284a6ee1dea264f6dc7d984
bethus@gmail.com <bethus@gmail.com@ad9a8d78-5211-11de-8950-a5d9c972bc64>
2013-12-19 23:31:29 +0000

    MS-LSAD partial implementation + testcases

---
+++ impacket/testcases/ImpactPacket/rundce.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+separator='======================================================================'
+#ls *.py | xargs -I{} --max-args=1 bash -c "echo -e '$separator\nExecuting: {}\n';python {}"
+#ls *.py | xargs --max-args=1 python
+
+export PYTHONPATH=../../..:$PYTHONPATH
+
+python test_scmr.py
+python test_epm.py
+python test_samr.py
+python test_wkst.py
+python test_srvs.py
+#python test_lsad.py
