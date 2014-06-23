e07ed49851310c75e3849a077e99b8a3d1656bfd
bethus@gmail.com <bethus@gmail.com@ad9a8d78-5211-11de-8950-a5d9c972bc64>
2014-06-23 18:14:15 +0000

    Added command line arguments to set up a share. Now it should be really easy to launch an SMB server :)

--- examples/smbserver.py
+++ examples/smbserver.py
@@ -13,14 +13,43 @@
 #  Alberto Solino <beto@coresecurity.com>
 #
 
-from impacket import smbserver
+import sys
+import argparse
+from impacket import smbserver, version
 
-server = smbserver.SimpleSMBServer()
-server.addShare('MYSHARE', '/tmp', 'MyComment')
+if __name__ == '__main__':
 
-# If you don't want log to stdout, comment the following line
-# If you want log dumped to a file, enter the filename
-server.setLogFile('')
+    print version.BANNER
 
-# Rock and roll
-server.start()
+    parser = argparse.ArgumentParser(add_help = False, description = "This script will launch a SMB Server and add a share specified as an argument. You need to be root in order to bind to port 445. No authentication will be enforced. Example: smbserver.py -comment 'My share' TMP /tmp")
+
+    parser.add_argument('shareName', action='store', help='name of the share to add')
+    parser.add_argument('sharePath', action='store', help='path of the share to add')
+    parser.add_argument('-comment', action='store', help='share\'s comment to display when asked for shares')
+
+    if len(sys.argv)==1:
+        parser.print_help()
+        sys.exit(1)
+
+    try:
+       options = parser.parse_args()
+    except Exception, e:
+       print e
+       sys.exit(1)
+
+    if options.comment is None:
+        comment = ''
+    else:
+        comment = options.comment
+
+    server = smbserver.SimpleSMBServer()
+
+    server.addShare(options.shareName.upper(), options.sharePath, comment)
+
+    # If you don't want log to stdout, comment the following line
+    # If you want log dumped to a file, enter the filename
+    server.setLogFile('')
+
+    # Rock and roll
+    server.start()
+
