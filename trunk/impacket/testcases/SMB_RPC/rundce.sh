e1f7a6e8c9dfb982a051b71089cb917368140f2a
bethus@gmail.com <bethus@gmail.com@ad9a8d78-5211-11de-8950-a5d9c972bc64>
2014-06-25 20:55:38 +0000

    1) Moving SMB-RPC to SMB_RPC 2) Changing inside dot11, test- for test_

---
+++ trunk/impacket/testcases/SMB_RPC/rundce.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+separator='======================================================================'
+#ls *.py | xargs -I{} --max-args=1 bash -c "echo -e '$separator\nExecuting: {}\n';python {}"
+#ls *.py | xargs --max-args=1 python
+
+export PYTHONPATH=../../..:$PYTHONPATH
+
+python test_rpcrt.py
+python test_scmr.py
+python test_epm.py
+python test_samr.py
+python test_wkst.py
+python test_srvs.py
+python test_lsad.py
+python test_lsat.py
+python test_rrp.py
