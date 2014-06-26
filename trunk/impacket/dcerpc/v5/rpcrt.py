4946b42d955ff18b875f4479b16430086e2f15f7
bethus@gmail.com <bethus@gmail.com@ad9a8d78-5211-11de-8950-a5d9c972bc64>
2014-06-26 13:54:47 +0000

    Updating reference to testcases

--- trunk/impacket/dcerpc/v5/rpcrt.py
+++ trunk/impacket/dcerpc/v5/rpcrt.py
@@ -6,7 +6,12 @@
 #
 # $Id$
 #
-# Partial C706.pdf + [MS-RPCE] implementation
+# Description:
+#   Partial C706.pdf + [MS-RPCE] implementation
+#
+#   Best way to learn how to use these calls is to grab the protocol standard
+#   so you understand what the call does, and then read the test case located
+#   at https://code.google.com/p/impacket/source/browse/#svn%2Ftrunk%2Fimpacket%2Ftestcases%2FSMB_RPC
 #
 # ToDo: 
 # [ ] Take out all the security provider stuff out of here (e.g. RPC_C_AUTHN_WINNT)
--- trunk/impacket/dcerpc/v5/rrp.py
+++ trunk/impacket/dcerpc/v5/rrp.py
@@ -11,6 +11,15 @@
 # Description:
 #   [MS-RRP] Interface implementation
 #
+#   Best way to learn how to use these calls is to grab the protocol standard
+#   so you understand what the call does, and then read the test case located
+#   at https://code.google.com/p/impacket/source/browse/#svn%2Ftrunk%2Fimpacket%2Ftestcases%2FSMB_RPC
+#
+#   Some calls have helper functions, which makes it even easier to use.
+#   They are located at the end of this file. 
+#   Helper functions start with "h"<name of the call>.
+#   There are test cases for them too. 
+#
 
 from impacket.dcerpc.v5 import ndr
 from impacket.dcerpc.v5.ndr import NDRCALL, NDR, NDRSTRUCT, NDRPOINTER, NDRUniConformantVaryingArray
