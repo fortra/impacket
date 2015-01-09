107b3bc6a162e5494fb3242be06161fc6c92a68c
bethus@gmail.com <bethus@gmail.com@ad9a8d78-5211-11de-8950-a5d9c972bc64>
2015-01-09 21:55:22 +0000

    Copyright update

--- examples/goldenPac.py
+++ examples/goldenPac.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# Copyright (c) 2003-2014 CORE Security Technologies
+# Copyright (c) 2003-2015 CORE Security Technologies
 #
 # This software is provided under under a slightly modified version
 # of the Apache Software License. See the accompanying LICENSE file
