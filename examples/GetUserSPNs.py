a1117e84ff2c300773d2faed72262f4558a03ef0
asolino <bethus@gmail.com>
2016-05-13 20:20:56 -0300

    Fixes small typo
    * It was copied from goldenPac.py

--- examples/GetUserSPNs.py
+++ examples/GetUserSPNs.py
@@ -275,7 +275,7 @@ if __name__ == '__main__':
     parser.add_argument('-request', action='store_true', default='False', help='Request TGS for users and output them in JtR/hashcat format (default False)')
     parser.add_argument('-outputfile', action='store',
                         help='Output filename to write ciphers in JtR/hashcat format')
-    parser.add_argument('-dc-ip', action='store',metavar = "ip address",  help='IP Address of the domain controller (needed to get the user''s SID). If ommited it use the domain part (FQDN) specified in the target parameter')
+    parser.add_argument('-dc-ip', action='store',metavar = "ip address",  help='IP Address of the domain controller. If ommited it use the domain part (FQDN) specified in the target parameter')
     parser.add_argument('-debug', action='store_true', help='Turn DEBUG output ON')
 
     group = parser.add_argument_group('authentication')
