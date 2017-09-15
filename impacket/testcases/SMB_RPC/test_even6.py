a11941947c9b238a56a0c0a76a56504ccede6088
asolino <bethus@gmail.com>
2017-09-15 12:18:35 -0300

    Adjusting test cases to new [MS-EVEN6] structure
    * Adding tests for helper functions as well. Some implemented functions still need test cases

--- impacket/testcases/SMB_RPC/test_even6.py
+++ impacket/testcases/SMB_RPC/test_even6.py
@@ -1,10 +1,18 @@
+###############################################################################
+#  Tested so far:
+#  EvtRpcRegisterLogQuery
+#  hEvtRpcRegisterLogQuery
+#  EvtRpcQueryNext
+#  hEvtRpcQueryNext
+###############################################################################
+
 import unittest
 import ConfigParser
 
 from impacket.dcerpc.v5 import transport
-from lib.impacket.dcerpc.v5 import epm, eventlog
+from impacket.dcerpc.v5 import epm, even6
 from impacket.dcerpc.v5.rpcrt import RPC_C_AUTHN_LEVEL_PKT_PRIVACY
-import hexdump
+from impacket.winregistry import hexdump
 
 
 class EVEN6Tests(unittest.TestCase):
@@ -22,19 +30,19 @@ class EVEN6Tests(unittest.TestCase):
         dce.set_auth_level(RPC_C_AUTHN_LEVEL_PKT_PRIVACY)
         dce.connect()
         if version == 1:
-            dce.bind(eventlog.MSRPC_UUID_EVENTLOG, transfer_syntax=self.ts)
+            dce.bind(even6.MSRPC_UUID_EVEN6, transfer_syntax=self.ts)
         else:
-            dce.bind(eventlog.MSRPC_UUID_EVENTLOG, transfer_syntax=self.ts)
+            dce.bind(even6.MSRPC_UUID_EVEN6, transfer_syntax=self.ts)
 
         return dce, rpctransport
 
     def test_EvtRpcRegisterLogQuery_EvtRpcQueryNext(self):
         dce, rpctransport = self.connect(2)
 
-        request = eventlog.EvtRpcRegisterLogQuery()
+        request = even6.EvtRpcRegisterLogQuery()
         request['Path'] = 'Security\x00'
         request['Query'] = '*\x00'
-        request['Flags'] = eventlog.EvtQueryChannelName | eventlog.EvtReadNewestToLowest
+        request['Flags'] = even6.EvtQueryChannelName | even6.EvtReadNewestToOldest
 
         request.dump()
         try:
@@ -45,7 +53,7 @@ class EVEN6Tests(unittest.TestCase):
 
         log_handle = resp['Handle']
 
-        request = eventlog.EvtRpcQueryNext()
+        request = even6.EvtRpcQueryNext()
         request['LogQuery'] = log_handle
         request['NumRequestedRecords'] = 5
         request['TimeOutEnd'] = 1000
@@ -62,7 +70,31 @@ class EVEN6Tests(unittest.TestCase):
             event_size = resp['EventDataSizes'][i]['Data']
             event = resp['ResultBuffer'][event_offset:event_offset + event_size]
             buff = ''.join([x.encode('hex') for x in event]).decode('hex')
-            print hexdump.hexdump(buff)
+            print hexdump(buff)
+
+    def test_hEvtRpcRegisterLogQuery_hEvtRpcQueryNext(self):
+        dce, rpctransport = self.connect(2)
+
+        try:
+            resp = even6.hEvtRpcRegisterLogQuery(dce, 'Security\x00', '*\x00', even6.EvtQueryChannelName | even6.EvtReadNewestToOldest)
+            resp.dump()
+        except Exception, e:
+            return
+
+        log_handle = resp['Handle']
+
+        try:
+            resp = even6.EvtRpcQueryNext(dce, log_handle, 5, 1000, 0)
+            resp.dump()
+        except Exception, e:
+            return
+
+        for i in range(resp['NumActualRecords']):
+            event_offset = resp['EventDataIndices'][i]['Data']
+            event_size = resp['EventDataSizes'][i]['Data']
+            event = resp['ResultBuffer'][event_offset:event_offset + event_size]
+            buff = ''.join([x.encode('hex') for x in event]).decode('hex')
+            print hexdump(buff)
 
 class SMBTransport(EVEN6Tests):
     def setUp(self):
@@ -103,7 +135,7 @@ class TCPTransport(EVEN6Tests):
         self.password = configFile.get('TCPTransport', 'password')
         self.machine = configFile.get('TCPTransport', 'machine')
         self.hashes = configFile.get('TCPTransport', 'hashes')
-        self.stringBinding = epm.hept_map(self.machine, eventlog.MSRPC_UUID_EVENTLOG, protocol='ncacn_ip_tcp')
+        self.stringBinding = epm.hept_map(self.machine, even6.MSRPC_UUID_EVEN6, protocol='ncacn_ip_tcp')
         self.ts = ('8a885d04-1ceb-11c9-9fe8-08002b104860', '2.0')
 
 class TCPTransport64(EVEN6Tests):
@@ -117,7 +149,7 @@ class TCPTransport64(EVEN6Tests):
         self.password = configFile.get('TCPTransport', 'password')
         self.machine = configFile.get('TCPTransport', 'machine')
         self.hashes = configFile.get('TCPTransport', 'hashes')
-        self.stringBinding = epm.hept_map(self.machine, eventlog.MSRPC_UUID_EVENTLOG, protocol='ncacn_ip_tcp')
+        self.stringBinding = epm.hept_map(self.machine, even6.MSRPC_UUID_EVEN6, protocol='ncacn_ip_tcp')
         self.ts = ('71710533-BEBA-4937-8319-B5DBEF9CCC36', '1.0')
 
 # Process command-line arguments.
@@ -128,5 +160,5 @@ if __name__ == '__main__':
         suite = unittest.TestLoader().loadTestsFromTestCase(globals()[testcase])
     else:
         suite = unittest.TestLoader().loadTestsFromTestCase(TCPTransport)
-        # suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TCPTransport64))
+        suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TCPTransport64))
     unittest.TextTestRunner(verbosity=1).run(suite)
