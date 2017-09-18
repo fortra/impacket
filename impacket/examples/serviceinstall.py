e7c83ec431f01486f9f89d3aa306df2286b0d730
asolino <bethus@gmail.com>
2017-09-18 11:17:11 -0300

    If serviceName is not specified, default to empty string.
    * This will force the class to generate a random string.
    * And mantains backward compatibility for those scripts that are not specifying a serviceName (e.g. goldenPac, smbrelayx) preventing them to break functionality.

--- impacket/examples/serviceinstall.py
+++ impacket/examples/serviceinstall.py
@@ -23,7 +23,7 @@ from impacket.smbconnection import SMBConnection
 from impacket.smb3structs import FILE_WRITE_DATA, FILE_DIRECTORY_FILE
 
 class ServiceInstall:
-    def __init__(self, SMBObject, exeFile, serviceName):
+    def __init__(self, SMBObject, exeFile, serviceName=''):
         self._rpctransport = 0
         self.__service_name = serviceName if len(serviceName) > 0  else  ''.join([random.choice(string.letters) for i in range(4)])
         self.__binary_service_name = ''.join([random.choice(string.letters) for i in range(8)]) + '.exe'
