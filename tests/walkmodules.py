c091f0e1e236a705806fa9eb0e108c66afb933b7
asolino <bethus@gmail.com>
2018-04-22 20:14:38 -0400

    If loading a module fails, continue

--- tests/walkmodules.py
+++ tests/walkmodules.py
@@ -6,4 +6,10 @@ package=impacket
 for importer, modname, ispkg in pkgutil.walk_packages(path=package.__path__,
                                                       prefix=package.__name__+'.',
                                                       onerror=lambda x: None):
-    __import__(modname)
+    try:
+        __import__(modname)
+    except Exception as e:
+        import traceback
+        traceback.print_exc()
+        print str(e)
+        pass
