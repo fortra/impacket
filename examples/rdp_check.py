9b07e5b0e980bfe87bafb2ad3f9c6326127a36e1
asolino <bethus@gmail.com>
2018-08-08 18:31:19 -0300

    Using TLSv1_2_METHOD and adding AES as cipher list for SSL connection
    * Also increasing the PyOpenSSL version required to match whatever is needed for this to work
    * Should address https://github.com/CoreSecurity/impacket/issues/429, testing is needed

--- examples/rdp_check.py
+++ examples/rdp_check.py
@@ -406,8 +406,8 @@ if __name__ == '__main__':
        # a self-signed X.509 certificate.
 
        # Switching to TLS now
-       ctx = SSL.Context(SSL.TLSv1_METHOD)
-       ctx.set_cipher_list('RC4')
+       ctx = SSL.Context(SSL.TLSv1_2_METHOD)
+       ctx.set_cipher_list('RC4,AES')
        tls = SSL.Connection(ctx,s)
        tls.set_connect_state()
        tls.do_handshake()
