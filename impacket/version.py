b64f8e35dd3f02c83804dec97cdd5a13816e49e5
asolino <bethus@gmail.com>
2019-01-30 18:19:06 -0300

    We're now in 2019 :)

--- impacket/version.py
+++ impacket/version.py
@@ -1,4 +1,4 @@
-# SECUREAUTH LABS. Copyright 2018 SecureAuth Corporation. All rights reserved.
+# SECUREAUTH LABS. Copyright 2019 SecureAuth Corporation. All rights reserved.
 #
 # This software is provided under under a slightly modified version
 # of the Apache Software License. See the accompanying LICENSE file
@@ -8,5 +8,5 @@
 VER_MAJOR = "0"
 VER_MINOR = "9.19-dev"
 
-BANNER = "Impacket v%s.%s - Copyright 2018 SecureAuth Corporation\n" % (VER_MAJOR,VER_MINOR)
+BANNER = "Impacket v%s.%s - Copyright 2019 SecureAuth Corporation\n" % (VER_MAJOR,VER_MINOR)
 
