a79c6032bc09f5f6b23bfd70ffb86ca7f385bdae
Dirkjan Mollema <dirkjan.mollema@fox-it.com>
2020-09-21 20:59:58 +0200

    actually that should be the original smb1 command

--- impacket/examples/ntlmrelayx/servers/smbrelayserver.py
+++ impacket/examples/ntlmrelayx/servers/smbrelayserver.py
@@ -699,13 +699,13 @@ class SMBRelayServer(Thread):
             if self.authUser == '/':
                 LOG.info('SMBD-%s: Connection from %s authenticated as guest (anonymous). Skipping target selection.' %
                          (connId, connData['ClientIP']))
-                return self.origsmb2TreeConnect (connId, smbServer, recvPacket)
+                return self.origsmbComTreeConnectAndX (connId, smbServer, recvPacket)
             self.target = self.targetprocessor.getTarget(identity = self.authUser)
             if self.target is None:
                 # No more targets to process, just let the victim to fail later
                 LOG.info('SMBD-%s: Connection from %s@%s controlled, but there are no more targets left!' %
                          (connId, self.authUser, connData['ClientIP']))
-                return self.origsmb2TreeConnect (connId, smbServer, recvPacket)
+                return self.origsmbComTreeConnectAndX (connId, smbServer, recvPacket)
 
             LOG.info('SMBD-%s: Connection from %s@%s controlled, attacking target %s://%s' % ( connId, self.authUser,
                                                         connData['ClientIP'], self.target.scheme, self.target.netloc))
