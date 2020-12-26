e852343adfee6b66075d3c94510661ccf7fab03e
mxrch <kvwaljam@protonmail.ch>
2020-12-26 02:50:15 +0100

    Added cat command to smbclient

--- impacket/examples/smbclient.py
+++ impacket/examples/smbclient.py
@@ -15,6 +15,7 @@
 #
 from __future__ import division
 from __future__ import print_function
+from io import BytesIO
 import sys
 import time
 import cmd
@@ -28,6 +29,8 @@ from impacket import LOG
 from impacket.smbconnection import SMBConnection, SMB2_DIALECT_002, SMB2_DIALECT_21, SMB_DIALECT, SessionError, \
     FILE_READ_DATA, FILE_SHARE_READ, FILE_SHARE_WRITE
 from impacket.smb3structs import FILE_DIRECTORY_FILE, FILE_LIST_DIRECTORY
+
+import chardet
 
 
 # If you wanna have readline like functionality in Windows, install pyreadline
@@ -459,6 +462,31 @@ class MiniImpacketShell(cmd.Cmd):
             os.remove(filename)
             raise
         fh.close()
+
+    def do_cat(self, filename):
+        if self.tid is None:
+            LOG.error("No share selected")
+            return
+        filename = filename.replace('/','\\')
+        fh = BytesIO()
+        pathname = ntpath.join(self.pwd,filename)
+        try:
+            self.smb.getFile(self.share, pathname, fh.write)
+        except:
+            raise
+        output = fh.getvalue()
+        encoding = chardet.detect(output)["encoding"]
+        error_msg = "[-] Output cannot be correctly decoded, are you sure the text is readable ?"
+        if encoding:
+            try:
+                print(output.decode(encoding))
+            except:
+                print(error_msg)
+            finally:
+                fh.close()
+        else:
+            print(error_msg)
+            fh.close()
 
     def do_close(self, line):
         self.do_logoff(line)
