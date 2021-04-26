b20aea22b15ee365e57bcb1446743f2449cf91cb
Yaroslav Kataev <35561605+zexusx26@users.noreply.github.com>
2021-04-26 07:51:01 +0500

    Update examples/wmiquery.py
    Co-authored-by: 0xdeaddood <56035084+0xdeaddood@users.noreply.github.com>

--- examples/wmiquery.py
+++ examples/wmiquery.py
@@ -130,7 +130,7 @@ if __name__ == '__main__':
     parser.add_argument('-namespace', action='store', default='//./root/cimv2', help='namespace name (default //./root/cimv2)')
     parser.add_argument('-file', type=argparse.FileType('r'), help='input file with commands to execute in the WQL shell')
     parser.add_argument('-debug', action='store_true', help='Turn DEBUG output ON')
-    parser.add_argument('-com-version', action='store', metavar = "MAJOR_VERSOIN:MINOR_VERSION", help='DCOM versoin, '
+    parser.add_argument('-com-version', action='store', metavar = "MAJOR_VERSION:MINOR_VERSION", help='DCOM version, '
                         'format is MAJOR_VERSOIN:MINOR_VERSION e.g. 5.7')
 
     group = parser.add_argument_group('authentication')
