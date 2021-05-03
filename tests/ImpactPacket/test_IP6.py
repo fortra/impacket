29016610c7d7de3697bd56212ad151cf60278918
Martin Gallo <mgallo@secureauth.com>
2021-05-03 13:38:55 -0700

    Tests: Removing deprecated assertEquals alias

--- tests/ImpactPacket/test_IP6.py
+++ tests/ImpactPacket/test_IP6.py
@@ -51,14 +51,14 @@ class TestIP6(unittest.TestCase):
         source_address = parsed_packet.get_ip_src()
         destination_address = parsed_packet.get_ip_dst()
         
-        self.assertEquals(protocol_version, 6, "IP6 parsing - Incorrect protocol version")
-        self.assertEquals(traffic_class, 72, "IP6 parsing - Incorrect traffic class")
-        self.assertEquals(flow_label, 148997, "IP6 parsing - Incorrect flow label")
-        self.assertEquals(payload_length, 1500, "IP6 parsing - Incorrect payload length")
-        self.assertEquals(next_header, 17, "IP6 parsing - Incorrect next header")
-        self.assertEquals(hop_limit, 1, "IP6 parsing - Incorrect hop limit")
-        self.assertEquals(source_address.as_string(), "FE80::78F8:89D1:30FF:256B", "IP6 parsing - Incorrect source address")
-        self.assertEquals(destination_address.as_string(), "FF02::1:3", "IP6 parsing - Incorrect destination address")
+        self.assertEqual(protocol_version, 6, "IP6 parsing - Incorrect protocol version")
+        self.assertEqual(traffic_class, 72, "IP6 parsing - Incorrect traffic class")
+        self.assertEqual(flow_label, 148997, "IP6 parsing - Incorrect flow label")
+        self.assertEqual(payload_length, 1500, "IP6 parsing - Incorrect payload length")
+        self.assertEqual(next_header, 17, "IP6 parsing - Incorrect next header")
+        self.assertEqual(hop_limit, 1, "IP6 parsing - Incorrect hop limit")
+        self.assertEqual(source_address.as_string(), "FE80::78F8:89D1:30FF:256B", "IP6 parsing - Incorrect source address")
+        self.assertEqual(destination_address.as_string(), "FF02::1:3", "IP6 parsing - Incorrect destination address")
         
     def test_creation(self):
         '''Test IP6 Packet creation.'''
@@ -72,7 +72,7 @@ class TestIP6(unittest.TestCase):
         crafted_packet.set_ip_src("FE80::78F8:89D1:30FF:256B")
         crafted_packet.set_ip_dst("FF02::1:3")
         crafted_buffer = crafted_packet.get_bytes().tolist()
-        self.assertEquals(crafted_buffer, self.binary_packet, "IP6 creation - Buffer mismatch")
+        self.assertEqual(crafted_buffer, self.binary_packet, "IP6 creation - Buffer mismatch")
 
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestIP6)
