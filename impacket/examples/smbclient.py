442fac9765d5da85f706e4cfc8eef5246a2c0475
deadjakk <deadjakk@shell.rip>
2021-06-26 12:22:59 -0500

    updated copyright year to 2021

--- impacket/examples/smbclient.py
+++ impacket/examples/smbclient.py
@@ -1,4 +1,4 @@
-# SECUREAUTH LABS. Copyright 2018 SecureAuth Corporation. All rights reserved.
+# SECUREAUTH LABS. Copyright 2021 SecureAuth Corporation. All rights reserved.
 #
 # This software is provided under under a slightly modified version
 # of the Apache Software License. See the accompanying LICENSE file
