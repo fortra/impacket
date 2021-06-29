bcc2f6433bc832fdd6cc5188ceadfda358813f86
kabutor <kabutor@gmail.com>
2021-06-29 11:11:46 +0200

    dpapi capi implementation

--- impacket/dpapi.py
+++ impacket/dpapi.py
@@ -1016,6 +1016,177 @@ class DPAPI_DOMAIN_RSA_MASTER_KEY(Structure):
         ('buffer', ':'),
     )
 
+# Private Decrypted Private Key 
+class PRIVATE_KEY_RSA(Structure):
+    structure = (
+        ('magic', '<L=0'),
+        ('len1', '<L=0'),
+        ('bitlen', '<L=0'),
+        ('unk', '<L=0'),
+        ('pubexp', '<L=0'),
+        ('_modulus', '_-modulus', 'self["len1"]'),
+        ('modulus', ':'),
+        ('_prime1', '_-prime1', 'self["len1"] // 2'),
+        ('prime1', ':'),
+        ('_prime2', '_-prime2', 'self["len1"] // 2'),
+        ('prime2', ':'),
+        ('_exponent1', '_-exponent1', 'self["len1"] // 2'),
+        ('exponent1', ':'),
+        ('_exponent2', '_-exponent2', 'self["len1"]// 2'),
+        ('exponent2', ':'),
+        ('_coefficient', '_-coefficient', 'self["len1"] // 2'),
+        ('coefficient', ':'),
+        ('_privateExponent', '_-privateExponent', 'self["len1"]'),
+        ('privateExponent', ':'),
+    )
+    def dump(self):
+        print("magic             : %s " % ( self['magic']))
+        print("len1              : %8x (%d)" % (self['len1'], self['len1']))
+        print("bitlen            : %8x (%d)" % (self['bitlen'], self['bitlen']))
+        print("pubexp            : %8x, (%d)" % (self['pubexp'], self['pubexp']))
+        print("modulus           : %s" % (hexlify( self['modulus'])))
+        print("prime1            : %s" % (hexlify( self['prime1'])))
+        print("prime2            : %s" % (hexlify( self['prime2'])))
+        print("exponent1         : %s" % (hexlify( self['exponent1'])))
+        print("exponent2         : %s" % (hexlify( self['exponent2'])))
+        print("coefficient       : %s" % (hexlify( self['coefficient'])))
+        print("privateExponent   : %s" % (hexlify( self['privateExponent'])))
+    def __init__(self, data = None, alignment = 0):
+        Structure.__init__(self, data, alignment)
+        chunk = int(self['bitlen'] / 16)
+        self['modulus']= self['modulus'][:chunk * 2]
+        self['prime1']= self['prime1'][:chunk]
+        self['prime2']= self['prime2'][:chunk]
+        self['exponent1']= self['exponent1'][:chunk]
+        self['exponent2']= self['exponent2'][:chunk]
+        self['coefficient']= self['coefficient'][:chunk]
+        self['privateExponent']= self['privateExponent'][:chunk * 2]
+
+# PVK DPAPI BLOB when it has the SIG data
+class PVKFile_SIG(Structure):
+    structure = (
+        ('Version', '<L=0'),
+        ('unk1', '<L=0'),
+        ('descrLen', '<L=0'),
+        ('SigHeadLen', "<L=0"),
+        ('SigPrivKeyLen', '<L=0'),
+        ('HeaderLen', '<L=0'),
+        ('PrivKeyLen', '<L=0'),
+        ('crcLen', '<L=0'),
+        ('SigFlagsLen', '<L=0'),
+        ('FlagsLen', '<L=0'),
+        ('_Description', '_-Description', 'self["descrLen"]'),
+        ('Description', ':'),
+        ('unk2', '<LLLLL=0'),
+        ('_Rsaheader_new', '_-Rsaheader_new', 'self["SigHeadLen"]' ),
+        ('Rsaheader_new', ':'),                                            
+        ('_Blob', '_-Blob', 'self["SigPrivKeyLen"]'),
+        ('Blob', ':', DPAPI_BLOB),
+        ('_ExportFlag', '_-ExportFlag', 'self["SigFlagsLen"]'),
+        ('ExportFlag', ':', DPAPI_BLOB), 
+
+
+    )
+    def dump(self):
+        print("[PVKFILE]")
+        print("[RSAHEADER]")
+        print("Version            : %8x (%d)" % (self['Version'], self['Version']))
+        print("descrLen           : %8x (%d)" % (self['descrLen'], self['descrLen'] ))
+        print("SigHeadLen         : %8x (%d)" % (self['SigHeadLen'], self['SigHeadLen']))
+        print("SigPrivKeyLen      : %8x (%d)" % (self['SigPrivKeyLen'], self['SigPrivKeyLen']))
+        print("HeaderLen          : %.8x (%d)" % (self['HeaderLen'], self['HeaderLen']))
+        print("PrivKeyLen         : %.8x (%d)" % (self['PrivKeyLen'], self['PrivKeyLen']))
+        print("crcLen             : %.8x (%d)" % (self['crcLen'], self['crcLen']))
+        print("SigFlagsLen        : %.8x (%d)" % (self['SigFlagsLen'], self['SigFlagsLen']))
+        print("FlagsLen           : %.8x (%d)" % (self['FlagsLen'], self['FlagsLen']))
+        print("Description   : %s" % (self['Description']))
+        print("Blank   : %s" % (self['unk2']))
+        print("RsaHeader : %s" %    (hexlify(self['Rsaheader_new']).decode('latin-1')))
+        print("[PRIVATE KEY]")
+        print (self['Blob'].dump())
+        print("[FLAGS]")
+        print (self['ExportFlag'].dump())
+
+# PVK DPAPI BLOB without SIG
+class PVKFile(Structure):
+    structure = (
+        ('Version', '<L=0'),
+        ('unk1', '<L=0'),
+        ('descrLen', '<L=0'),
+        ('SigHeadLen', "<L=0"),
+        ('SigPrivKeyLen', '<L=0'),
+        ('HeaderLen', '<L=0'),
+        ('PrivKeyLen', '<L=0'),
+        ('crcLen', '<L=0'),
+        ('SigFlagsLen', '<L=0'),
+        ('FlagsLen', '<L=0'),
+        ('_Description', '_-Description', 'self["descrLen"]'),
+        ('Description', ':'),
+        ('unk2', '<LLLLL=0'),
+        ('_Rsaheader_new', '_-Rsaheader_new', 'self["HeaderLen"]' ),
+        ('Rsaheader_new', ':'),                                            
+        ('_Blob', '_-Blob', 'self["PrivKeyLen"]'),
+        ('Blob', ':', DPAPI_BLOB),
+        ('_ExportFlag', '_-ExportFlag', 'self["FlagsLen"]'),
+        ('ExportFlag', ':', DPAPI_BLOB), 
+
+
+    )
+    def dump(self):
+        print("[PVKFILE]")
+        print("[RSAHEADER]")
+        print("Version            : %8x (%d)" % (self['Version'], self['Version']))
+        print("descrLen           : %8x (%d)" % (self['descrLen'], self['descrLen'] ))
+        print("SigHeadLen         : %8x (%d)" % (self['SigHeadLen'], self['SigHeadLen']))
+        print("SigPrivKeyLen      : %8x (%d)" % (self['SigPrivKeyLen'], self['SigPrivKeyLen']))
+        print("HeaderLen          : %.8x (%d)" % (self['HeaderLen'], self['HeaderLen']))
+        print("PrivKeyLen         : %.8x (%d)" % (self['PrivKeyLen'], self['PrivKeyLen']))
+        print("crcLen             : %.8x (%d)" % (self['crcLen'], self['crcLen']))
+        print("SigFlagsLen        : %.8x (%d)" % (self['SigFlagsLen'], self['SigFlagsLen']))
+        print("FlagsLen           : %.8x (%d)" % (self['FlagsLen'], self['FlagsLen']))
+        print("Description   : %s" % (self['Description']))
+        print("Blank   : %s" % (self['unk2']))
+        print("RsaHeader : %s" %    (hexlify(self['Rsaheader_new']).decode('latin-1')))
+        print("[PRIVATE KEY]")
+        print (self['Blob'].dump())
+        print("[FLAGS]")
+        print (self['ExportFlag'].dump())
+
+# This class is the same as the previous two, its only used to see wich one of the previous clasess we will use
+# sorry 
+class PVKHeader(Structure):
+    structure = (
+        ('Version', '<L=0'),
+        ('unk1', '<L=0'),
+        ('descrLen', '<L=0'),
+        ('SigHeadLen', "<L=0"),
+        ('SigPrivKeyLen', '<L=0'),
+        ('HeaderLen', '<L=0'),
+        ('PrivKeyLen', '<L=0'),
+        ('crcLen', '<L=0'),
+        ('SigFlagsLen', '<L=0'),
+        ('FlagsLen', '<L=0'),
+        ('_Description', '_-Description', 'self["descrLen"]'),
+        ('Description', ':'),
+        ('unk2', '<LLLLL=0'),
+        
+        ('Remaining', ':'),
+
+    )
+    def dump(self):
+        print("[PVKFILE]")
+        print("[RSAHEADER]")
+        print("Version            : %8x (%d)" % (self['Version'], self['Version']))
+        print("descrLen           : %8x (%d)" % (self['descrLen'], self['descrLen'] ))
+        print("SigHeadLen         : %8x (%d)" % (self['SigHeadLen'], self['SigHeadLen']))
+        print("SigPrivKeyLen      : %8x (%d)" % (self['SigPrivKeyLen'], self['SigPrivKeyLen']))
+        print("HeaderLen          : %.8x (%d)" % (self['HeaderLen'], self['HeaderLen']))
+        print("PrivKeyLen         : %.8x (%d)" % (self['PrivKeyLen'], self['PrivKeyLen']))
+        print("crcLen             : %.8x (%d)" % (self['crcLen'], self['crcLen']))
+        print("SigFlagsLen        : %.8x (%d)" % (self['SigFlagsLen'], self['SigFlagsLen']))
+        print("FlagsLen           : %.8x (%d)" % (self['FlagsLen'], self['FlagsLen']))
+        print("Description   : %s" % (self['Description']))
+
 def privatekeyblob_to_pkcs1(key):
     '''
     parse private key into pkcs#1 format
@@ -1036,3 +1207,25 @@ def privatekeyblob_to_pkcs1(key):
 
     r = RSA.construct((modulus, pubExp, privateExp, prime1, prime2))
     return r
+
+# Function to convert the decrypted DPAPI data into a Cryptodome RsaKey
+def pvkblob_to_pkcs1(key):
+    '''
+    modified from impacket dpapi.py
+    parse private key into pkcs#1 format
+    :param key:
+    :return:
+    '''
+    modulus = bytes_to_long(key['modulus'][::-1]) # n
+    prime1 = bytes_to_long(key['prime1'][::-1]) # p
+    prime2 = bytes_to_long(key['prime2'][::-1]) # q
+    exp1 = bytes_to_long(key['exponent1'][::-1])
+    exp2 = bytes_to_long(key['exponent2'][::-1])
+    coefficient = bytes_to_long(key['coefficient'][::-1])
+    privateExp = bytes_to_long(key['privateExponent'][::-1]) # d
+    pubExp = int(key['pubexp']) # e
+    # RSA.Integer(prime2).inverse(prime1) # u
+
+    r = RSA.construct((modulus, pubExp, privateExp, prime1, prime2))
+    return r
+
