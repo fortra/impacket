93fb6ec903854b4512bb8ff56eb5022c47b178d9
CCob <CCob@users.noreply.github.com>
2021-10-17 21:22:43 +0100

    Initial support for raw NTLM relay server for lsarelayx (and other third party relay tools)

--- impacket/examples/ntlmrelayx/servers/__init__.py
+++ impacket/examples/ntlmrelayx/servers/__init__.py
@@ -9,3 +9,4 @@
 from impacket.examples.ntlmrelayx.servers.httprelayserver import HTTPRelayServer
 from impacket.examples.ntlmrelayx.servers.smbrelayserver import SMBRelayServer
 from impacket.examples.ntlmrelayx.servers.wcfrelayserver import WCFRelayServer
+from impacket.examples.ntlmrelayx.servers.rawrelayserver import RAWRelayServer
