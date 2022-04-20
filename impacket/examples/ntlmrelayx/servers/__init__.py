f699f100b20fe5e097214a60611ac9a9a740f7e3
MrAle98 <ale18109800@gmail.com>
2022-04-20 17:44:35 +0200

    Revert "refactor"
    This reverts commit 49129761c9416f93a9c35f558fd0aab2438ef947.

--- impacket/examples/ntlmrelayx/servers/__init__.py
+++ impacket/examples/ntlmrelayx/servers/__init__.py
@@ -9,5 +9,6 @@
 from impacket.examples.ntlmrelayx.servers.httprelayserver import HTTPRelayServer
 from impacket.examples.ntlmrelayx.servers.smbrelayserver import SMBRelayServer
 from impacket.examples.ntlmrelayx.servers.rpcrelayserver import RPCRelayServer
+from impacket.examples.ntlmrelayx.servers.rpcrelayserver import RPCRelayServer
 from impacket.examples.ntlmrelayx.servers.wcfrelayserver import WCFRelayServer
 from impacket.examples.ntlmrelayx.servers.rawrelayserver import RAWRelayServer
