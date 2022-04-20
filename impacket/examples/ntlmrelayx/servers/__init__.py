437af8083af9f6be4a75463de9ec68491aad2d2f
MrAle98 <ale18109800@gmail.com>
2022-04-20 17:44:36 +0200

    Revert "Added rogue oxid resolver. Added rpc relay server."
    This reverts commit 40e20b4fbf2c1976dec591d02488b353a378ab61.

--- impacket/examples/ntlmrelayx/servers/__init__.py
+++ impacket/examples/ntlmrelayx/servers/__init__.py
@@ -8,7 +8,5 @@
 #
 from impacket.examples.ntlmrelayx.servers.httprelayserver import HTTPRelayServer
 from impacket.examples.ntlmrelayx.servers.smbrelayserver import SMBRelayServer
-from impacket.examples.ntlmrelayx.servers.rpcrelayserver import RPCRelayServer
-from impacket.examples.ntlmrelayx.servers.rpcrelayserver import RPCRelayServer
 from impacket.examples.ntlmrelayx.servers.wcfrelayserver import WCFRelayServer
 from impacket.examples.ntlmrelayx.servers.rawrelayserver import RAWRelayServer
