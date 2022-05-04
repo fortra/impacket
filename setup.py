d509775976ba37f4eaea630cc511e2fc3b65aba3
0xdeaddood <leandrocuozzo@hotmail.com>
2022-05-04 12:00:02 -0300

    And going back to dev version

--- setup.py
+++ setup.py
@@ -21,8 +21,8 @@ PACKAGE_NAME = "impacket"
 
 VER_MAJOR = 0
 VER_MINOR = 10
-VER_MAINT = 0
-VER_PREREL = ""
+VER_MAINT = 1
+VER_PREREL = "dev1"
 try:
     if call(["git", "branch"], stderr=STDOUT, stdout=open(os.devnull, 'w')) == 0:
         p = Popen("git log -1 --format=%cd --date=format:%Y%m%d.%H%M%S", shell=True, stdin=PIPE, stderr=PIPE, stdout=PIPE)
@@ -51,7 +51,7 @@ def read(fname):
 
 setup(
     name=PACKAGE_NAME,
-    version="{}.{}.{}".format(VER_MAJOR, VER_MINOR, VER_MAINT),
+    version="{}.{}.{}.{}{}".format(VER_MAJOR, VER_MINOR, VER_MAINT, VER_PREREL, VER_LOCAL),
     description="Network protocols Constructors and Dissectors",
     url="https://www.secureauth.com/labs/open-source-tools/impacket",
     author="SecureAuth Corporation",
