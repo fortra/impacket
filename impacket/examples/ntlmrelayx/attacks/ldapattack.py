fortra/impacket

commit 63438ae7aad6eeb35035a0e8cb0f7e01d6b82372

Update ldapattack.py (#1690)
Reorder attack components so a computer account is created before the delegate attack is attempted.

impacket/examples/ntlmrelayx/attacks/ldapattack.py
@@ -1105,11 +1105,6 @@ class LDAPAttack(ProtocolAttack):
             if dns_name_ok and dns_ipaddr_ok:
                 self.addDnsRecord(name, ipaddr)
 
-        # Perform the Delegate attack if it is enabled and we relayed a computer account
-        if self.config.delegateaccess and self.username[-1] == '$':
-            self.delegateAttack(self.config.escalateuser, self.username, domainDumper, self.config.sid)
-            return
-
         # Add a new computer if that is requested
         # privileges required are not yet enumerated, neither is ms-ds-MachineAccountQuota
         if self.config.addcomputer is not None:
@@ -1124,6 +1119,11 @@ class LDAPAttack(ProtocolAttack):
             self.addComputer(computerscontainer, domainDumper)
             return
 
+        # Perform the Delegate attack if it is enabled and we relayed a computer account
+        if self.config.delegateaccess and self.username[-1] == '$':
+            self.delegateAttack(self.config.escalateuser, self.username, domainDumper, self.config.sid)
+            return
+        
         # Perform the Shadow Credentials attack if it is enabled
         if self.config.IsShadowCredentialsAttack:
             self.shadowCredentialsAttack(domainDumper)

commit 337d50d04e191c2b323ccf4877500fc487126dd1

Update bug introduced in #1690. Continue with attacks after addComputer is completed

impacket/examples/ntlmrelayx/attacks/ldapattack.py
@@ -1117,7 +1117,6 @@ class LDAPAttack(ProtocolAttack):
             ][0]
             LOG.debug("Computer container is {}".format(computerscontainer))
             self.addComputer(computerscontainer, domainDumper)
-            return
 
         # Perform the Delegate attack if it is enabled and we relayed a computer account
         if self.config.delegateaccess and self.username[-1] == '$':