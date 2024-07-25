9f36a10e5b9bbf5ba0a5b7469ecd12f1947c8cbf
adrian manrique <60896207+anadrianmanrique@users.noreply.github.com>
2024-07-25 12:57:04 -0300

    Update setup.py
    updated supported python versions

--- setup.py
+++ setup.py
@@ -73,10 +73,11 @@ setup(
     extras_require={'pyreadline:sys_platform=="win32"': [],
                     },
     classifiers=[
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.6",
+
     ]
 )
