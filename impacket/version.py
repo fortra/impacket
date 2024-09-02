8898194531fa1393201d0c8ea8499a9978e42c61
Gabriel Gonzalez <gabriel.gonzalez@fortra.com>
2024-09-02 10:46:45 -0300

    Update version.py
    Updated copyright aligned with rest of the files..

--- impacket/version.py
+++ impacket/version.py
@@ -19,7 +19,7 @@ except pkg_resources.DistributionNotFound:
     version = "?"
     print("Cannot determine Impacket version. "
           "If running from source you should at least run \"python setup.py egg_info\"")
-BANNER = "Impacket v{} - Copyright 2023 Fortra\n".format(version)
+BANNER = "Impacket v{} - Copyright Fortra, LLC and its affiliated companies \n".format(version)
 WARNING_BANNER = "".join(("===============================================================================\n",
                           "  Warning: This functionality will be deprecated in the next Impacket version  \n", 
                           "===============================================================================\n"))
