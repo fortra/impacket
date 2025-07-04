dd7048e460a8d9ab65d9321785af321528d98f07
ke0 <ke0@ke0.com>
2025-07-04 15:39:47 +0800

    Add fallback to TLSv1 for LDAPS connection compatibility

--- impacket/examples/ntlmrelayx/clients/ldaprelayclient.py
+++ impacket/examples/ntlmrelayx/clients/ldaprelayclient.py
@@ -23,6 +23,7 @@ import sys
 from struct import unpack
 from impacket import LOG
 from ldap3 import Server, Connection, ALL, NTLM, MODIFY_ADD, Tls
+from ldap3.core.exceptions import LDAPSocketOpenError
 import ssl
 from ldap3.operation import bind
 try:
@@ -173,8 +174,14 @@ class LDAPSRelayClient(LDAPRelayClient):
         LDAPRelayClient.__init__(self, serverConfig, target, targetPort, extendedSecurity)
 
     def initConnection(self):
-        tls_config = Tls(ciphers='ALL', version=ssl.PROTOCOL_TLSv1)
-        self.server = Server("ldaps://%s:%s" % (self.targetHost, self.targetPort), get_info=ALL, tls=tls_config)
-        self.session = Connection(self.server, user="a", password="b", authentication=NTLM)
-        self.session.open(False)
+        try:
+            tls_config = Tls(ciphers='ALL:@SECLEVEL=0', version=ssl.PROTOCOL_TLSv1_2)
+            self.server = Server("ldaps://%s:%s" % (self.targetHost, self.targetPort), get_info=ALL, tls=tls_config)
+            self.session = Connection(self.server, user="a", password="b", authentication=NTLM)
+            self.session.open(False)
+        except LDAPSocketOpenError:
+            tls_config = Tls(ciphers='ALL:@SECLEVEL=0', version=ssl.PROTOCOL_TLSv1)
+            self.server = Server("ldaps://%s:%s" % (self.targetHost, self.targetPort), get_info=ALL, tls=tls_config)
+            self.session = Connection(self.server, user="a", password="b", authentication=NTLM)
+            self.session.open(False)
         return True
