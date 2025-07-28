5ddad9f660126fc0c3cbae91f1e1ff811fc00b08
Ryan Kleffman <91687869+ryanq47@users.noreply.github.com>
2025-07-28 11:06:15 -0700

    GetUserSPN's update to test machineonly flag

--- examples/GetUserSPNs.py
+++ examples/GetUserSPNs.py
@@ -458,7 +458,7 @@ if __name__ == '__main__':
     parser.add_argument('-stealth', action='store_true', help='Removes the (servicePrincipalName=*) filter from the LDAP query for added stealth. '
                                                               'May cause huge memory consumption / errors on large domains.')
     parser.add_argument('-usersfile', help='File with user per line to test')
-    parser.add_argument('-machineonly', metavar='machineonly', action='store_true', default=False, help='Queries for machine accounts only')
+    parser.add_argument('-machineonly', action='store_true', default=False, help='Queries for machine accounts only')
     parser.add_argument('-request-machine', metavar='machinename', help='Filters down to one machine to request. Helpful in larger domains. Example: `workstation01$`')
 
 
@@ -523,7 +523,7 @@ if __name__ == '__main__':
         options.request = True
 
     # auto enable machineonly flag on machinename flag being enabled.
-    if options.machinename is not None or options.machineonly is False:
+    if options.request_machine is not None:
         options.machineonly = True
 
     try:
