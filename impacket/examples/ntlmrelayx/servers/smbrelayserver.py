4165c3ffefbc63d438f59227b287673bb3bed8e0
Roman Karwacik <roman.karwacik@redteam-pentesting.de>
2026-02-03 09:17:39 +0100

    smbrelayserver.py: Dont advertise support for NEGOEX

--- impacket/examples/ntlmrelayx/servers/smbrelayserver.py
+++ impacket/examples/ntlmrelayx/servers/smbrelayserver.py
@@ -213,8 +213,7 @@ class SMBRelayServer(Thread):
         respSMBCommand['SecurityBufferOffset'] = 0x80
 
         blob = SPNEGO_NegTokenInit()
-        blob['MechTypes'] = [TypesMech['NEGOEX - SPNEGO Extended Negotiation Security Mechanism'],
-                             TypesMech['NTLMSSP - Microsoft NTLM Security Support Provider']]
+        blob['MechTypes'] = [TypesMech['NTLMSSP - Microsoft NTLM Security Support Provider']]
 
 
         respSMBCommand['Buffer'] = blob.getData()
@@ -257,8 +256,7 @@ class SMBRelayServer(Thread):
            if len(blob['MechTypes'][0]) > 0:
                # Is this GSSAPI NTLM or something else we don't support?
                mechType = blob['MechTypes'][0]
-               if mechType != TypesMech['NTLMSSP - Microsoft NTLM Security Support Provider'] and \
-                               mechType != TypesMech['NEGOEX - SPNEGO Extended Negotiation Security Mechanism']:
+               if mechType != TypesMech['NTLMSSP - Microsoft NTLM Security Support Provider']:
                    # Nope, do we know it?
                    if mechType in MechTypes:
                        mechStr = MechTypes[mechType]
