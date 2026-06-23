91292c03b10857f4689160acb6d6b9c73b5e91df
n3rada <72791564+n3rada@users.noreply.github.com>
2026-06-23 05:54:33 +0000

    fix(tds): decode BIGCHARTYPE and TEXTTYPE bytes as latin-1 string

--- impacket/tds.py
+++ impacket/tds.py
@@ -2202,8 +2202,12 @@ class MSSQL:
                     charLen = struct.unpack("<L", data[: struct.calcsize("<L")])[0]
                     data = data[struct.calcsize("<L") :]
                     if charLen != 0xFFFF:
-                        value = data[:charLen]
+                        raw = data[:charLen]
                         data = data[charLen:]
+                        try:
+                            value = raw.decode("latin-1")
+                        except UnicodeDecodeError:
+                            value = raw.decode("utf-8", errors="replace")
                     else:
                         value = None
 
@@ -2330,11 +2334,14 @@ class MSSQL:
                     value = None
 
             elif _type == TDS_BIGCHARTYPE:
-                # print "BIGC"
                 charLen = struct.unpack("<H", data[: struct.calcsize("<H")])[0]
                 data = data[struct.calcsize("<H") :]
-                value = data[:charLen]
+                raw = data[:charLen]
                 data = data[charLen:]
+                try:
+                    value = raw.decode("latin-1")
+                except UnicodeDecodeError:
+                    value = raw.decode("utf-8", errors="replace")
 
             elif _type == TDS_INT8TYPE:
                 value = struct.unpack("<q", data[:8])[0]
